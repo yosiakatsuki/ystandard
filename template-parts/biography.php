f82e6886bc210b11d7f7605e0d568ca69b73a90d
Yoshiaki Ogata <yosi@yosi.local>
2016-11-06 06:24:07 +0900

    Add biography styles

--- template-parts/biography.php
+++ template-parts/biography.php
@@ -1,15 +1,20 @@
-<div class="author-info">
-	<div class="author-avatar">
-		<?php ys_image_the_user_avatar(); ?>
-	</div><!-- .author-avatar -->
+<div class="author-info clearfix">
+	<?php
+		$avatar = ys_image_get_the_user_avatar_img();
+		if($avatar != ''):
+	?>
+	<figure class="author-avatar">
+		<?php echo $avatar; ?>
+	</figure><!-- .author-avatar -->
+	<?php endif;  ?>
 
-	<div class="author-description">
+	<div class="author-description<?php echo $avatar !== '' ? ' show-avatar' : ''; ?>">
 		<h2 class="author-title">
 			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php ys_entry_the_entry_author(); ?></a>
 		</h2>
 
-		<p class="author-bio">
-			<?php the_author_meta( 'description' ); ?>
-		</p><!-- .author-bio -->
+		<div class="author-bio">
+			<?php echo wpautop(str_replace(array("\r\n", "\r", "\n"),"\n\n",get_the_author_meta( 'description' ))); ?>
+		</div><!-- .author-bio -->
 	</div><!-- .author-description -->
 </div><!-- .author-info -->
