74aa1caee013ff07e8d4fdfe4c0511c82174408c
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-02-14 17:24:49 +0900

    Fix:広告用ウィジェット　マークアップ調整

--- inc/widgets.php
+++ inc/widgets.php
@@ -268,18 +268,14 @@ class YS_AD_Text_Widget extends WP_Widget {
 	public function widget( $args, $instance ) {
 		global $wp_query;
 
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : '';
 		$text = ! empty( $instance['text'] ) ? $instance['text'] : '';
 
 		$text = apply_filters('ys_ad_text_widget_text', $text, $instance, $this );
 
 		if(!is_404() && !(is_search() && 0 == $wp_query->found_posts) && '' !== $text ) {
-			echo $args['before_widget'];
-			if ( ! empty( $instance['title'] ) ) {
-				echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
-			}
+			echo '<div class="ys-ad-widget">';
 			echo $instance['text'];
-			echo $args['after_widget'];
+			echo '</div>';
 		}
 
 	}
@@ -292,13 +288,8 @@ class YS_AD_Text_Widget extends WP_Widget {
 	 * @param array $instance データベースからの前回保存された値
 	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : '';
 		$text = ! empty( $instance['text'] ) ? $instance['text'] : '';
 		?>
-		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'タイトル:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-		</p>
 		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>">内容:</label>
 		<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo esc_textarea($text); ?></textarea></p>
 		<?php
@@ -316,7 +307,6 @@ class YS_AD_Text_Widget extends WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 		if ( current_user_can( 'unfiltered_html' ) ) {
 			$instance['text'] = $new_instance['text'];
 		} else {
