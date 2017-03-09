b71a7d54ee660a12368a190a00a51377c8f537ed
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-03-09 23:01:42 +0900

    fix #22 一覧ページのHTML5エラー対処

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,5 +1,6 @@
-<a class="entry-container" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark" itemprop="url">
+<a class="entry-container" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark">
 	<article id="post-<?php the_ID(); ?>" <?php post_class( array('post-list','clearfix') ); ?> itemscope itemtype="http://schema.org/BlogPosting" itemref="masthead">
+		<meta itemprop="url" content="<?php the_permalink(); ?>" />
 		<?php get_template_part('template-parts/content','list-detail'); ?>
 	</article><!-- #post-## -->
 </a>
