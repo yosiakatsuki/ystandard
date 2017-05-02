776fe74bb89470b796103984aca3b354fd1496bd
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-05-02 12:43:15 +0900

    fix authorカード調整

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -494,7 +494,7 @@ if (!function_exists( 'ys_template_get_the_entry_author')) {
 			$href = '';
 		}
 
-		$author = '<span class="author vcard"'.$stracture.'><'.$tag.' class="url fn n" '.$href.'><span'.$itempropname.'>'.$author_name.'</span></'.$tag.'></span>';
+		$author = '<span class="vcard author"'.$stracture.'><'.$tag.' class="url fn" '.$href.'><span'.$itempropname.'>'.$author_name.'</span></'.$tag.'></span>';
 
 		if( $ico ){
 			$author = '<svg class="entry-meta-ico" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>'.$author;
