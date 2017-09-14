cf6c3495ae1888c353cce37c74512295c0a0fae9
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-09-14 14:13:14 +0900

    refs #101 著者情報ウィジェットにユーザー指定モードを追加

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -14,20 +14,35 @@ function ys_shortcode_author($args) {
 		// デフォルト値をセットして、変数に代入
 		extract(shortcode_atts(array(
 				'default_user_name' => false,
+				'user_name' => false,
 		), $args));
 
 		$author_id = '';
-		if(is_singular()){
+
+		/**
+		 * 個別投稿であれば投稿者を取得
+		 */
+		if( is_singular() ){
 			$author_id = get_the_author_meta( 'ID' );
 		}
 
-		if($author_id == '' && $default_user_name !== false){
-			$user = get_user_by( 'slug', $default_user_name );
-			if($user){
+		/**
+		 * user_nameが設定されていればユーザー名からauthor_idを取得
+		 */
+		if( $user_name !== false ) {
+			$user = get_user_by( 'slug', $user_name );
+			if( $user ){
 				$author_id = $user->ID;
 			}
 		}
-		if($author_id == ''){
+
+		if( $author_id == '' && $default_user_name !== false ){
+			$user = get_user_by( 'slug', $default_user_name );
+			if( $user ){
+				$author_id = $user->ID;
+			}
+		}
+		if( $author_id == '' ){
 			return '';
 		}
 
