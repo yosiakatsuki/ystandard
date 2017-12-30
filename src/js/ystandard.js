9c43ce587c8d10236e07602559e516e09a853344
yosiakatsuki <y.ogata@yosiakatsuki.net>
2017-12-30 23:16:12 +0900

    jsファイル作成

--- src/js/ystandard.js
+++ src/js/ystandard.js
@@ -1,1 +1,50 @@
+import YSLoadScript from './modules/load-script'
 
+let loadScript = new YSLoadScript()
+
+/**
+ * グローバル変数
+ */
+let ysSetTimeoutId = null
+
+/**
+ * スクロール処理メイン
+ */
+function ysScroll () {
+  /**
+   * css,js 遅延ロード
+   */
+  loadScript.lazyLoadCss()
+  loadScript.lazyLoadScripts()
+}
+
+/**
+ * 初期化
+ */
+function ysInit () {
+  /**
+   * スクロール処理追加
+   */
+  window.addEventListener('scroll', () => {
+    if (ysSetTimeoutId) {
+      return false
+    }
+
+    ysSetTimeoutId = setTimeout(() => {
+      ysScroll()
+      ysSetTimeoutId = null
+    }, 100)
+  })
+
+  /**
+   * スクリプト読み込み
+   */
+  loadScript.loadScripts()
+}
+
+/**
+ * イベント追加
+ */
+window.addEventListener('load', () => {
+  ysInit()
+})
