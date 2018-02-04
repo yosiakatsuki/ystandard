78a8598078e2c113d40ffe77a524b1f2cd2f5e8a
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-02-05 06:46:31 +0900

    add フッターSNS

--- template-parts/nav/global-nav.php
+++ template-parts/nav/global-nav.php
@@ -18,7 +18,7 @@
 		<?php
 			wp_nav_menu( array(
 				'theme_location' => 'global',
-				'menu_class'     => 'global-nav__menu row row--align-center list-style--none',
+				'menu_class'     => 'global-nav__menu row flex--a-center list-style--none',
 				'menu_id'        => 'global-nav__menu',
 				'container'      => false,
 				'depth'          => 2,
