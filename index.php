5b691261422a188bf6913046b44f0b61fcd87b74
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-02-08 11:01:15 +0900

    update copy archive -> index

--- index.php
+++ index.php
@@ -1,41 +1,31 @@
 <?php
-
 get_header(); ?>
-
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" >
-
+<div class="container">
+	<section class="content-area content__wrap row">
+		<main id="main" class="site-main content__main">
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php if(is_singular()) : ?>
-					<h1 class="page-title"><?php single_post_title(); ?></h1>
-				<?php else: ?>
-					<?php
-						the_archive_title( '<h2 class="page-title">', '</h2>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-					?>
-				<?php endif; ?>
-			</header>
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			// ページネーション
-			ys_pagination();
-
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+			<header class="page-header archive__header">
+				<?php
+					the_archive_title( '<h2 class="page-title archive__title">', '</h2>' );
+					the_archive_description( '<div class="taxonomy-description archive__dscr color__font-sub">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+			<div class="archive__list">
+				<?php
+					while ( have_posts() ) : the_post();
+						get_template_part( 'template-parts/content/archive' );
+					endwhile;
+					// ページネーション
+					ys_pagination();
+				?>
+			</div><!-- .archive__list -->
+		<?php
+			else :
+				get_template_part( 'template-parts/content/none' );
+			endif;
 		?>
-
 		</main><!-- .site-main -->
+		<?php get_sidebar(); ?>
 	</section><!-- .content-area -->
-
-<?php get_sidebar(); ?>
+</div><!-- .container -->
 <?php get_footer(); ?>
