a2ca8bed43c7732769308dea2346e2114b004f5b
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-02-21 10:40:48 +0900

    fix AMP global nav styles

--- inc/amp/amp-footer.php
+++ inc/amp/amp-footer.php
@@ -16,13 +16,13 @@ add_action( 'ys_amp_footer', 'ys_the_json_ld' );
 
 function ys_the_amp_slider() {
 	?>
-	<amp-sidebar id="sidebar" layout="nodisplay" side="right" class="amp-slider">
-		<button class="global-nav__btn" on="tap:sidebar.close">
+	<amp-sidebar id="sidebar" layout="nodisplay" side="right" class="amp-slider color__nav-bg--sp">
+		<button class="global-nav__btn global-nav__amp-btn--close" on="tap:sidebar.close">
 			<span class="top"></span>
 			<span class="middle"></span>
 			<span class="bottom"></span>
 		</button>
-		<nav id="global-nav" class="global-nav color__nav-bg--sp">
+		<nav id="global-nav--amp" class="global-nav--amp color__nav-bg--sp">
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'global',
