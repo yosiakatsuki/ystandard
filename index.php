1bf8391b7582381c5a8aba9a797477974c939aef
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-03-04 06:05:30 +0900

    fix archiveと同じ動きにする

--- index.php
+++ index.php
@@ -6,26 +6,28 @@
  */
 get_header(); ?>
 <div class="container">
-	<section class="content-area content__wrap row">
+	<div class="content-area content__wrap">
 		<main id="main" class="site-main content__main">
 		<?php if ( have_posts() ) : ?>
 			<header class="page-header archive__header">
 				<?php
-					the_archive_title( '<h2 class="page-title archive__title">', '</h2>' );
+					the_archive_title( '<h2 class="page-title archive__title clear-headline">', '</h2>' );
 					the_archive_description( '<div class="taxonomy-description archive__dscr color__font-sub">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 			<div class="archive__list">
 				<?php
 					while ( have_posts() ) : the_post();
-						get_template_part( 'template-parts/content/archive' );
+						get_template_part( 'template-parts/content/archive', ys_get_option( 'ys_archive_type' ) );
 					endwhile;
-					/**
-					 * ページネーション
-					 */
-					get_template_part( 'template-parts/pagination/pagination' );
 				?>
 			</div><!-- .archive__list -->
+			<?php
+				/**
+				 * ページネーション
+				 */
+				get_template_part( 'template-parts/pagination/pagination' );
+				?>
 		<?php
 			else :
 				get_template_part( 'template-parts/content/none' );
@@ -33,6 +35,6 @@ get_header(); ?>
 		?>
 		</main><!-- .site-main -->
 		<?php get_sidebar(); ?>
-	</section><!-- .content-area -->
+	</div><!-- .content-area -->
 </div><!-- .container -->
 <?php get_footer(); ?>
