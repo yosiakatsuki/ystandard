b11f450d4cc8e8d529ea35247c5726ae6e03d297
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-03-10 23:17:40 +0900

    add 投稿者一覧ショートコード

---
+++ inc/shortcode/shortcode-author-list.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * ショートコード: 投稿者一覧
+ *
+ * @package ystandard
+ * @author yosiakatsuki
+ * @license GPL-2.0+
+ */
+
+/**
+ * 投稿者一覧出力
+ *
+ * @param array $args パラメーター.
+ * @return string
+ */
+function ys_shortcode_author_list( $args ) {
+	$html = '';
+	$args = shortcode_atts(
+		array(
+			'id'      => '',
+			'exclude' => '',
+		),
+		$args
+	);
+	/**
+	 * ユーザーデータ取得パラメータ
+	 */
+	$user_args = array(
+		'orderby' => 'ID',
+		'order'   => 'ASC',
+	);
+	/**
+	 * 初期値セット
+	 */
+	$id = $args['id'];
+	if ( '' !== $args['exclude'] ) {
+		$user_args = wp_parse_args(
+			array( 'exclude' => explode( ',', $args['exclude'] ) ),
+			$user_args
+		);
+	}
+	/**
+	 * ユーザー一覧取得
+	 */
+	$users = get_users( $user_args );
+	foreach ( $users as $user ) {
+		global $ys_author;
+		$ys_author_temp = $ys_author;
+		$ys_author      = $user->ID;
+		ob_start();
+		get_template_part( 'template-parts/author/profile-box' );
+		$html .= apply_filters( 'ys_shortcode_author_list_html', ob_get_clean(), $ys_author, $id );
+		/**
+		 * 戻す
+		 */
+		$ys_author = $ys_author_temp;
+	}
+	$html = sprintf( '<div class="ys_author_list author--2col">%s</div>', $html );
+	return apply_filters( 'ys_shortcode_author_list', $html, $id );
+}
+add_shortcode( 'ys_author_list', 'ys_shortcode_author_list' );
--- inc/shortcode/shortcode-author.php
+++ inc/shortcode/shortcode-author.php
@@ -1,72 +1,75 @@
 <?php
 /**
+ * ショートコード: 投稿者
+ *
  * @package ystandard
  * @author yosiakatsuki
  * @license GPL-2.0+
  */
-/**
- *
- * ショートコード: 投稿者
- *
- */
+
 /**
  * 投稿者表示
+ *
+ * @param array $args パラメータ.
  */
 function ys_shortcode_author( $args ) {
-		/**
-		 * デフォルト値をセット
-		 */
-		$args = shortcode_atts(
-								array(
-										'default_user_name' => false,
-										'user_name'         => false,
-								),
-								$args
-						);
-		$default_user_name = $args['default_user_name'];
-		$user_name = $args['user_name'];
-		$author_id = '';
-		/**
-		 * 個別投稿であれば投稿者を取得
-		 */
-		if( is_singular() ){
-			$author_id = get_the_author_meta( 'ID' );
+	$args = shortcode_atts(
+		array(
+			'default_user_name' => false,
+			'user_name'         => false,
+		),
+		$args
+	);
+	/**
+	 * デフォルト値をセット
+	 */
+	$default_user_name = $args['default_user_name'];
+	$user_name         = $args['user_name'];
+	$author_id         = '';
+	/**
+	 * 個別投稿であれば投稿者を取得
+	 */
+	if ( is_singular() ) {
+		$author_id = get_the_author_meta( 'ID' );
+	}
+	/**
+	 * User_nameが設定されていればユーザー名からauthor_idを取得
+	 */
+	if ( false !== $user_name ) {
+		$user = get_user_by( 'slug', $user_name );
+		if ( $user ) {
+			$author_id = $user->ID;
 		}
-		/**
-		 * user_nameが設定されていればユーザー名からauthor_idを取得
-		 */
-		if( $user_name !== false ) {
-			$user = get_user_by( 'slug', $user_name );
-			if( $user ){
-				$author_id = $user->ID;
-			}
+	}
+	/**
+	 * デフォルトユーザーが指定されていれば表示する
+	 */
+	if ( '' === $author_id && false !== $default_user_name ) {
+		$user = get_user_by( 'slug', $default_user_name );
+		if ( $user ) {
+			$author_id = $user->ID;
 		}
-		/**
-		 * デフォルトユーザーが指定されていれば表示する
-		 */
-		if( '' === $author_id && false !== $default_user_name ){
-			$user = get_user_by( 'slug', $default_user_name );
-			if( $user ){
-				$author_id = $user->ID;
-			}
-		}
-		$author_id = apply_filters( 'ys_shortcode_author_id', $author_id );
-		/**
-		 * ユーザー指定が無ければ空白
-		 */
-		if( '' === $author_id ){
-			return '';
-		}
-		global $ys_author;
-		$ys_author_temp = $ys_author;
-		$ys_author = $author_id;
-		/**
-		 * バッファリング
-		 */
-		ob_start();
-		get_template_part( 'template-parts/author/profile-box' );
-		$html = ob_get_clean();
-		$ys_author = $ys_author_temp;
-		return apply_filters( 'ys_shortcode_author_html', $html, $author_id );
+	}
+	$author_id = apply_filters( 'ys_shortcode_author_id', $author_id );
+	/**
+	 * ユーザー指定が無ければ空白
+	 */
+	if ( '' === $author_id ) {
+		return '';
+	}
+	global $ys_author;
+	$ys_author_temp = $ys_author;
+	$ys_author      = $author_id;
+	/**
+	 * バッファリング
+	 */
+	ob_start();
+	get_template_part( 'template-parts/author/profile-box' );
+	$html = ob_get_clean();
+	/**
+	 * 投稿者情報を戻す
+	 */
+	$ys_author = $ys_author_temp;
+	return apply_filters( 'ys_shortcode_author_html', $html, $author_id );
 }
 add_shortcode( 'ys_author', 'ys_shortcode_author' );
