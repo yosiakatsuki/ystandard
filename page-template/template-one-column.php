728502ab927963e019f4e3ce4abe42785f799960
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-03-14 13:33:48 +0900

    fix php cs

--- page-template/template-one-column.php
+++ page-template/template-one-column.php
@@ -3,24 +3,30 @@
  * Template Name:1カラム
  * Template Post Type:post,page
  * Description:ワンカラムテンプレート
+ *
+ * @package ystandard
+ * @author yosiakatsuki
+ * @license GPL-2.0+
  */
+
 get_header(); ?>
 <div class="container">
 	<div class="content-area content__wrap">
 		<main id="main" class="site-main content__main">
-			<?php do_action( 'ys_site_main_prepend' ) ?>
+			<?php do_action( 'ys_site_main_prepend' ); ?>
 			<?php
-			while ( have_posts() ) : the_post();
-				if( is_page() ){
+			while ( have_posts() ) :
+				the_post();
+				if ( is_page() ) {
 					get_template_part( 'template-parts/content/page' );
 				} else {
 					get_template_part( 'template-parts/content/single' );
 				}
 			endwhile;
 			?>
-			<?php do_action( 'ys_site_main_append' ) ?>
+			<?php do_action( 'ys_site_main_append' ); ?>
 		</main><!-- .site-main -->
-		<?php //get_sidebar(); ?>
+		<?php // get_sidebar. ?>
 	</div><!-- .content-area -->
 </div><!-- .container -->
 <?php get_footer(); ?>
