f0a70c1072493f4d10262f2ddfab063763335aa2
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-03-18 16:52:26 +0900

    fix phpcs

--- inc/amp/amp-filter.php
+++ inc/amp/amp-filter.php
@@ -29,29 +29,35 @@ if ( ! function_exists( 'ys_amp_convert_content' ) ) {
 add_filter( 'the_content', 'ys_amp_convert_content', 11 );
 
 if ( ! function_exists( 'ys_amp_convert_all' ) ) {
+	/**
+	 * AMPフォーマットへ変換
+	 *
+	 * @param  string $content content.
+	 * @return string
+	 */
 	function ys_amp_convert_all( $content ) {
 			/**
 		 * HTMLタグなどの置換
 		 */
 		$content = ys_amp_convert_html( $content );
 		/**
-		 * sns埋め込みの置換
+		 * SNS埋め込みの置換
 		 */
 		$content = ys_amp_convert_sns( $content );
 		/**
-		 * imgの置換
+		 * Imgの置換
 		 */
 		$content = ys_amp_convert_image( $content );
 		/**
-		 * iframeの置換
+		 * Iframeの置換
 		 */
 		$content = ys_amp_convert_iframe( $content );
 		/**
-		 * scriptの削除
+		 * Scriptの削除
 		 */
 		$content = ys_amp_delete_script( $content );
 		/**
-		 * styleの削除
+		 * Styleの削除
 		 */
 		$content = ys_amp_delete_style( $content );
 		return $content;
