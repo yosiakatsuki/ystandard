95c844165530078d86dee81603d83ec36ffbf793
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-05-30 13:28:32 +0900

    ref #141 ウィジェットベースクラスの整理

--- inc/class/widgets/class-ys-widget-base.php
+++ inc/class/widgets/class-ys-widget-base.php
@@ -12,19 +12,6 @@
  */
 class YS_Widget_Base extends WP_Widget {
 	/**
-	 * 投稿一覧取得
-	 *
-	 * @param array    $args パラメータ.
-	 * @param string   $id ID.
-	 * @param string   $thumbnail_size サムネイルサイズ.
-	 * @param WP_Query $query クエリ直接指定.
-	 * @return string
-	 */
-	protected function get_ys_post_list( $args = array(), $id = '', $thumbnail_size = '', $query = null ) {
-		$ys_post_list = new YS_Post_List( $id, $thumbnail_size, $query );
-		return $ys_post_list->get_post_list( $args );
-	}
-	/**
 	 * テーマ内で使える画像サイズ取得
 	 */
 	protected function get_image_sizes() {
