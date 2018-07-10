71dae6da3a4ffe72ba6e38ac68384f7ae886025e
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-07-10 23:06:18 +0900

    fix #161 記事上下ウィジェット追加

--- inc/customizer/customizer-page.php
+++ inc/customizer/customizer-page.php
@@ -131,4 +131,59 @@ function ys_customizer_page_add_settings( $wp_customize ) {
 			'section' => 'ys_customizer_section_page',
 		)
 	);
+	/**
+	 * 記事前後のウィジェット表示設定
+	 */
+	$ys_customizer->add_label( array(
+		'id'          => 'ys_page_content_widget_label',
+		'label'       => '記事前後のウィジェット表示設定',
+		'description' => '記事前後に表示するウィジェットの設定',
+		'section'     => 'ys_customizer_section_page',
+	) );
+	/**
+	 * 記事上ウィジェットを出力する
+	 */
+	$ys_customizer->add_checkbox(
+		array(
+			'id'      => 'ys_show_page_before_content_widget',
+			'default' => 0,
+			'label'   => '記事上ウィジェットを出力する',
+			'section' => 'ys_customizer_section_page',
+		)
+	);
+	/**
+	 * 記事上ウィジェットの優先順位
+	 */
+	$ys_customizer->add_number(
+		array(
+			'id'          => 'ys_page_before_content_widget_priority',
+			'default'     => 10,
+			'label'       => '記事上ウィジェットの優先順位',
+			'description' => '記事上ウィジェットの優先順位。1~99を目安に設定して下さい。（初期値10）数字が小さいほどコンテンツに近い位置にウィジェットが表示されます。（他プラグインで出力している内容との表示順調整用）',
+			'section'     => 'ys_customizer_section_page',
+		)
+	);
+	/**
+	 * 記事下ウィジェットを出力する
+	 */
+	$ys_customizer->add_checkbox(
+		array(
+			'id'      => 'ys_show_page_after_content_widget',
+			'default' => 0,
+			'label'   => '記事下ウィジェットを出力する',
+			'section' => 'ys_customizer_section_page',
+		)
+	);
+	/**
+	 * 記事下ウィジェットの優先順位
+	 */
+	$ys_customizer->add_number(
+		array(
+			'id'          => 'ys_page_after_content_widget_priority',
+			'default'     => 10,
+			'label'       => '記事下ウィジェットの優先順位',
+			'description' => '記事下ウィジェットの優先順位。1~99を目安に設定して下さい。（初期値10）数字が小さいほどコンテンツに近い位置にウィジェットが表示されます。（他プラグインで出力している内容との表示順調整用）',
+			'section'     => 'ys_customizer_section_page',
+		)
+	);
 }
--- inc/widgets/widgets.php
+++ inc/widgets/widgets.php
@@ -3,7 +3,7 @@
  * ウィジェト
  *
  * @package ystandard
- * @author yosiakatsuki
+ * @author  yosiakatsuki
  * @license GPL-2.0+
  */
 
@@ -34,18 +34,32 @@ function ys_widget_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	/**
+	 * 記事上下エリア
+	 */
+	register_sidebar( array(
+		'name'          => '記事上エリア',
+		'id'            => 'before-content',
+		'description'   => '記事直上に表示されるウィジェット(表示する場合は投稿設定・固定ページ設定で記事上ウィジェットを有効にして下さい)',
+		'before_widget' => '<div id="%1$s" class="content-widget before-content-widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 	/**
 	 * 記事下CTAエリア
 	 */
 	register_sidebar( array(
 		'name'          => '記事下エリア',
-		'id'            => 'entry-footer',
-		'description'   => '記事直下に表示されるウィジェット',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'id'            => 'after-content',
+		'description'   => '記事直下に表示されるウィジェット(表示する場合は投稿設定・固定ページ設定で記事上ウィジェットを有効にして下さい)',
+		'before_widget' => '<div id="%1$s" class="content-widget after-content-widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
 	/**
 	 * フッター
 	 */
@@ -76,15 +90,116 @@ function ys_widget_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+	
 }
+
 add_action( 'widgets_init', 'ys_widget_init' );
 
 /**
  * ウィジェットの登録
  */
 function ys_widgets_register_widget() {
-		register_widget( 'YS_Ranking_Widget' );
-		register_widget( 'YS_AD_Text_Widget' );
-		register_widget( 'YS_Taxonomy_Posts_Widget' );
+	register_widget( 'YS_Ranking_Widget' );
+	register_widget( 'YS_AD_Text_Widget' );
+	register_widget( 'YS_Taxonomy_Posts_Widget' );
 }
+
 add_action( 'widgets_init', 'ys_widgets_register_widget' );
+
+/**
+ * コンテンツ前のウィジェット
+ *
+ * @param string $content コンテンツ.
+ *
+ * @return mixed
+ */
+function ys_before_content_widget( $content ) {
+	if ( ! is_singular() || is_front_page() ) {
+		return $content;
+	}
+	if ( ys_is_active_before_content_widget() ) {
+		ob_start();
+		dynamic_sidebar( 'before-content' );
+		$content = ys_amp_convert_content( ob_get_clean() ) . $content;
+	}
+
+	return $content;
+}
+
+/**
+ * コンテンツ後のウィジェット
+ *
+ * @param string $content コンテンツ.
+ *
+ * @return mixed
+ */
+function ys_after_content_widget( $content ) {
+	if ( ! is_singular() || is_front_page() ) {
+		return $content;
+	}
+	if ( ys_is_active_after_content_widget() ) {
+		ob_start();
+		dynamic_sidebar( 'after-content' );
+		$content = $content . ys_amp_convert_content( ob_get_clean() );
+	}
+
+	return $content;
+}
+
+
+/**
+ * コンテンツ前後のウィジェットをセットする。
+ */
+function ys_set_content_widget() {
+	if ( ! is_singular() ) {
+		return;
+	}
+	add_filter(
+		'the_content',
+		'ys_before_content_widget',
+		ys_get_before_content_widget_priority()
+	);
+
+	add_filter(
+		'the_content',
+		'ys_after_content_widget',
+		ys_get_after_content_widget_priority()
+	);
+
+}
+
+add_action( 'wp', 'ys_set_content_widget' );
+
+/**
+ * 記事上ウィジェットの優先順位
+ *
+ * @return int
+ */
+function ys_get_before_content_widget_priority() {
+	$priority = 10;
+	if ( is_single() ) {
+		$priority = ys_get_option( 'ys_post_before_content_widget_priority' );
+	}
+	if ( is_page() ) {
+		$priority = ys_get_option( 'ys_page_before_content_widget_priority' );
+	}
+
+	return apply_filters( 'ys_get_before_content_widget_priority', $priority );
+}
+
+/**
+ * 記事下ウィジェットの優先順位
+ *
+ * @return int
+ */
+function ys_get_after_content_widget_priority() {
+	$priority = 10;
+	if ( is_single() ) {
+		$priority = ys_get_option( 'ys_post_after_content_widget_priority' );
+	}
+	if ( is_page() ) {
+		$priority = ys_get_option( 'ys_page_after_content_widget_priority' );
+	}
+
+	return apply_filters( 'ys_get_after_content_widget_priority', $priority );
+}
