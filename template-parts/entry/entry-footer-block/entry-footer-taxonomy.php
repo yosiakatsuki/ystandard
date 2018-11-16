ccf4de058f8fbc9ecb52a0d99d8d1261469266b7
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-11-17 06:18:21 +0900

    fix #191 カスタム投稿でカテゴリーなしでもアイコンだけ表示されている問題対処

--- template-parts/entry/entry-footer-block/entry-footer-taxonomy.php
+++ template-parts/entry/entry-footer-block/entry-footer-taxonomy.php
@@ -10,16 +10,20 @@
 if ( ! ys_get_option( 'ys_show_post_category' ) ) {
 	return;
 }
+$category = ys_get_the_category();
+$tags     = ys_get_the_tag_list( ' /' );
+if ( empty( $category ) && empty( $tags ) ) {
+	return;
+}
 ?>
 <aside class="entry__footer-taxonomy">
+	<?php if ( ! empty( $category ) ) : ?>
 	<div class="entry__footer-category flex">
 		<h2 class="entry__footer-tax-title clear-headline"><i class="fa fa-folder-o entry__footer-tax-icon" aria-hidden="true"></i>Category :</h2>
-		<p class="entry__footer-tax-container color__font-sub"><?php ys_the_category_list( ' /' ); ?></p>
+		<p class="entry__footer-tax-container color__font-sub"><?php ys_the_array_implode( $category, ' /' ); ?></p>
 	</div><!-- .entry__footer-category -->
-	<?php
-	$tags = ys_get_the_tag_list( ' /' );
-	if ( $tags ) :
-		?>
+	<?php endif; ?>
+	<?php if ( $tags ) : ?>
 		<div class="entry__footer-tag flex">
 			<h2 class="entry__footer-tax-title clear-headline"><i class="fa fa-tag entry__footer-tax-icon" aria-hidden="true"></i>Tags :</h2>
 			<p class="entry__footer-tax-container color__font-sub"><?php echo $tags; ?></p>
