29598b28b3f33e3ccdfc06fad931af9b17123988
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-04-04 23:47:29 +0900

    公開日　アイコン周りの調整

--- template-parts/singular/singular-date.php
+++ template-parts/singular/singular-date.php
@@ -19,14 +19,14 @@ $format = get_option( 'date_format' ); ?>
 		 * 公開日のみ
 		 */
 		?>
-		<span class="singular-date__published"><i class="far fa-calendar singular-date__icon"></i><time class="updated" datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time( $format ); ?></time></span>
+		<span class="singular-date__published"><i class="far fa-calendar icon-l"></i><time class="updated" datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time( $format ); ?></time></span>
 	<?php else : ?>
 		<?php
 		/**
 		 * 公開日・更新日
 		 */
 		?>
-		<span class="singular-date__published"><i class="far fa-calendar singular-date__icon"></i><?php the_time( $format ); ?></span>
-		<span class="singular-date__update"><i class="fas fa-redo-alt singular-date__icon"></i><time class="updated" datetime="<?php the_modified_time( 'Y-m-d' ); ?>"><?php the_modified_time( $format ); ?></time></span>
+		<span class="singular-date__published"><i class="far fa-calendar icon-l"></i><?php the_time( $format ); ?></span>
+		<span class="singular-date__update"><i class="fas fa-redo-alt icon-l"></i><time class="updated" datetime="<?php the_modified_time( 'Y-m-d' ); ?>"><?php the_modified_time( $format ); ?></time></span>
 	<?php endif; ?>
 </div><!-- .entry__date -->
