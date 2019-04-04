6a2759710367555b48676e9e161894bdd8d1a705
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-04-04 23:40:49 +0900

    フル幅サムネイルとカスタムヘッダー機能調整

--- inc/enqueue/enqueue-customizer-custom-header.php
+++ inc/enqueue/enqueue-customizer-custom-header.php
@@ -13,100 +13,34 @@
  * @return string
  */
 function ys_get_customizer_inline_css_custom_header() {
-	if ( ! ys_is_active_custom_header() ) {
+	if ( ! ys_is_active_custom_header() || ys_is_full_width_thumbnail() ) {
 		return '';
 	}
-
-	$css     = <<<EOD
-
-.wp-custom-header {
-	position: relative;
-}
-
-.wp-custom-header img {
-	display: block;
-	height: auto;
-	width: 100%;
-	margin: 0 auto;
-}
-
-.ys-custom-header--video .wp-custom-header {
-	width: 100%;
-	padding-top: 56.25%;
-}
-
-.ys-custom-header--video .wp-custom-header video,
-.ys-custom-header--video .wp-custom-header iframe {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-}
-
-.wp-custom-header-video-button {
-	display: block;
-	position: absolute;
-	left: .5em;
-	bottom: .5em;
-	padding: .5em;
-	border: 1px solid rgba(255, 255, 255, 0.3);
-	background-color: rgba(0, 0, 0, 0.3);
-	font-size: .8em;
-	line-height: 1;
-	color: rgba(255, 255, 255, 0.3);
-}
-
-@media screen and (min-width: 960px) {
-
-.custom-header--full .site-header {
-	width: 100%;
-	position: absolute;
-	top: 0;
-	left: 0;
-	z-index: 11;
-	background-color: '#bg_color#';
-	color: '#text_color#';
-}
-
-.custom-header--full .color__site-header,
-.custom-header--full .color__site-title,
-.custom-header--full .color__site-title:hover,
-.custom-header--full .color__site-dscr {
-	color: '#text_color#';
-}
-
-.custom-header--full .color__nav-font--pc {
-	color: '#text_color#';
-}
-
-.custom-header--full .color__nav-bg--pc,
-.custom-header--full .global-nav__sub-menu li {
-	background-color: transparent;
-}
-
-.custom-header--full .global-nav__item:not(.menu-item-has-children):hover {
-	border-bottom-color: '#text_color#';
-}
-
-.custom-header--full.admin-bar .site-header {
-	top: 32px;
-}
-
-}
-
-
-
-EOD;
-	$opacity = ys_get_option( 'ys_wp_header_media_full_opacity' );
-	$opacity = $opacity / 100;
+	$opacity    = ys_get_option( 'ys_wp_header_media_full_opacity' );
+	$opacity    = $opacity / 100;
+	$text_color = '#fff';
+	$bg_color   = 'rgba(0,0,0,' . $opacity . ')';
 	if ( 'light' == ys_get_option( 'ys_wp_header_media_full_type' ) ) {
-		$css = str_replace( '\'#text_color#\'', '#222', $css );
-		$css = str_replace( '\'#bg_color#\'', 'rgba(255,255,255,' . $opacity . ')', $css );
-	} else {
-		$css = str_replace( '\'#text_color#\'', '#fff', $css );
-		$css = str_replace( '\'#bg_color#\'', 'rgba(0,0,0,' . $opacity . ')', $css );
+		$text_color = '#222';
+		$bg_color   = 'rgba(255,255,255,' . $opacity . ')';
 	}
-
+	$css = <<<EOD
+.custom-header--full .site-header,
+.custom-header--full .h-nav.rwd li:hover ul {
+  background-color: ${bg_color};
+}
+.custom-header--full .h-nav.rwd li:hover:not(.has-child) {
+  border-bottom-color: ${text_color};
+}
+.custom-header--full .site-header,
+.custom-header--full .header__title a,
+.custom-header--full .header__title a:hover,
+.custom-header--full .header__dscr,
+.custom-header--full .h-nav.rwd .h-nav__main a,
+.custom-header--full .h-nav.rwd .h-nav__main a:hover {
+  color: ${text_color};
+}
+EOD;
+	
 	return apply_filters( 'ys_get_customizer_inline_css_custom_header', $css );
 }
