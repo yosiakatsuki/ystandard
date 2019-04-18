95e61fd6b130612c39ca5e4bc78a9a4523152a67
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-04-18 10:40:58 +0900

    フッターSNSリストレイアウト調整

--- template-parts/footer/footer-sns.php
+++ template-parts/footer/footer-sns.php
@@ -13,16 +13,18 @@ if ( empty( $sns_list ) ) {
 }
 ?>
 <div class="footer-sns">
-	<ul class="footer-sns__list li-clear flex-wrap flex--j-center">
+	<ul class="footer-sns__list li-clear flex flex-wrap flex--j-center">
 		<?php
 		foreach ( $sns_list as $value ) :
-			if ( '' != $value['url'] ) :
+			if ( '' !== $value['url'] ) :
 				?>
 				<li class="footer-sns__item <?php echo $value['class']; ?>">
-					<a class="footer-sns__link flex flex--c-c" href="<?php echo $value['url']; ?>" target="_blank" rel="nofollow">
-						<?php if ( '' != $value['icon-class'] ) : ?>
-							<i class="<?php echo $value['icon-class']; ?>" aria-hidden="true"></i>
-						<?php endif; ?>
+					<a class="footer-sns__link flex flex--c-c" href="<?php echo $value['url']; ?>" target="_blank" rel="nofollow" title="<?php echo $value['title']; ?>">
+						<?php
+						if ( '' !== $value['icon'] ) {
+							echo $value['icon'];
+						}
+						?>
 					</a>
 				</li>
 				<?php
