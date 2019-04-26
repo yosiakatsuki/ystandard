423981af3a3ed1a4e4e748fb48f7e2a5af767408
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-04-26 11:16:36 +0900

    詳細ページ　ページャーテンプレート整理

--- template-parts/page/content.php
+++ template-parts/page/content.php
@@ -15,16 +15,7 @@
 		<div class="entry-content entry__content">
 			<?php
 			the_content();
-			wp_link_pages(
-				array(
-					'before'      => '<nav class="page-links pagination flex flex--j-center">',
-					'after'       => '</nav>',
-					'link_before' => '<span class="page-links__item pagination__item flex flex--c-c">',
-					'link_after'  => '</span>',
-					'pagelink'    => '<span class="page-links__text">%</span>',
-					'separator'   => '',
-				)
-			);
+			get_template_part( 'template-parts/page/pagination' );
 			?>
 		</div><!-- .entry-content -->
 		<?php get_template_part( 'template-parts/page/footer' ); ?>
---
+++ template-parts/page/pagination.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 詳細ページ用ページネーション
+ */
+
+/**
+ * 実態は下記テンプレート
+ * template-parts/singular/pagination.php
+ * 固定ページだけカスタマイズする場合はこのテンプレートを子テーマにコピーして書き換えてください。
+ */
+get_template_part( 'template-parts/singular/pagination' );
