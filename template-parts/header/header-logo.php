809b44af64cce49f8b2b9a2e252be138441f56f4
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-04-26 11:10:24 +0900

    404テンプレート整理

--- template-parts/header/header-logo.php
+++ template-parts/header/header-logo.php
@@ -15,7 +15,7 @@
 	 */
 	$logo  = ys_get_header_logo();
 	$class = 'site-title header__title';
-	if ( is_front_page() ) {
+	if ( is_front_page() || is_404() ) {
 		printf( '<h1 class="%s clear-h">%s</h1>', $class, $logo );
 	} else {
 		printf( '<div class="%s clear-h">%s</div>', $class, $logo );
