da93bacf6d85f733f9a92da23b3782755b7940ad
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-06-10 19:35:53 +0900

    検索フォームスタイル追加

--- searchform.php
+++ searchform.php
@@ -10,7 +10,7 @@
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<input type="search" class="search-field color__font-sub" placeholder="<?php _e( 'Search' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<input type="search" class="search-field" placeholder="<?php _e( 'Search' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
-	<button type="submit" class="search-submit color__font-sub"><i class="fas fa-search"></i></button>
+	<button type="submit" class="search-submit"><i class="fas fa-search"></i></button>
 </form>
