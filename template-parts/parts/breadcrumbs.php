ea9a270741898651cdf244745b02c91e6d7a4919
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-06-21 13:45:24 +0900

    構造化データ調整

--- template-parts/parts/breadcrumbs.php
+++ template-parts/parts/breadcrumbs.php
@@ -3,7 +3,7 @@
  * パンくずリストテンプレート
  *
  * @package ystandard
- * @author yosiakatsuki
+ * @author  yosiakatsuki
  * @license GPL-2.0+
  */
 
@@ -14,19 +14,19 @@ $items = ys_get_breadcrumbs();
 ?>
 <div id="breadcrumbs" class="breadcrumbs">
 	<div class="container">
-		<ol class="breadcrumbs__list li-clear color__font-sub" itemscope itemtype="http://schema.org/BreadcrumbList">
+		<ol class="breadcrumbs__list li-clear" itemscope itemtype="https://schema.org/BreadcrumbList">
 			<?php foreach ( $items as $key => $item ) : ?>
-				<li class="breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+				<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="breadcrumbs__item">
 					<?php if ( empty( $item['link'] ) ) : ?>
-						<span itemscope itemtype="http://schema.org/Thing" itemprop="item">
+						<span itemtype="https://schema.org/Thing" itemprop="item">
 							<span itemprop="name"><?php echo esc_html( $item['title'] ); ?></span>
 						</span>
 					<?php else : ?>
-						<a class="color__font-sub" itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo esc_url( $item['link'] ); ?>">
+						<a itemtype="https://schema.org/Thing" itemprop="item" href="<?php echo esc_url( $item['link'] ); ?>">
 							<span itemprop="name"><?php echo esc_html( $item['title'] ); ?></span>
 						</a>
 					<?php endif; ?>
-					<meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>" />
+					<meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>"/>
 				</li>
 			<?php endforeach; ?>
 		</ol>
