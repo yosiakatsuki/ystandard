2f4635ee1045694cc31baf1f65a66b0882a29166
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-06-23 14:17:10 +0900

    投稿：アイキャッチ表示・非表示の調整

--- template-parts/header/custom-header.php
+++ template-parts/header/custom-header.php
@@ -10,6 +10,9 @@
 if ( ! ys_is_active_custom_header() && ! ys_is_full_width_thumbnail() ) {
 	return;
 }
+if ( is_singular() && ! ys_is_active_post_thumbnail() ) {
+	return;
+}
 ?>
 <div class="ys-custom-header ys-custom-header--<?php echo esc_attr( ys_get_custom_header_type() ); ?>">
 	<?php ys_the_custom_header_markup(); ?>
