a05a4213b3b297d24b7d931429e2e0e0f46f7133
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-10-01 18:48:58 +0900

    アドミンバー用スタイルシートの読み込み不具合修正

--- inc/enqueue/enqueue.php
+++ inc/enqueue/enqueue.php
@@ -115,7 +115,7 @@ function ys_set_enqueue_css() {
 	if ( is_admin_bar_showing() ) {
 		ys_enqueue_css(
 			'adminbar-css',
-			get_stylesheet_directory_uri() . '/css/ystandard-adminbar.css',
+			get_template_directory_uri() . '/css/ystandard-adminbar.css',
 			false,
 			array(),
 			ys_get_theme_version( true )
