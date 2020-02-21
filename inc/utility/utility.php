0b783a70eab527961da0c127319b87ddad0a76c5
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-02-21 23:17:41 +0900

    utility整理

--- inc/utility/utility.php
+++ inc/utility/utility.php
@@ -12,6 +12,20 @@
  */
 require_once dirname( __FILE__ ) . '/class-ys-utility.php';
 
+/**
+ * 投稿タイプ取得
+ */
+function ys_get_post_type() {
+	global $wp_query;
+	$post_type = get_post_type();
+	if ( ! $post_type ) {
+		if ( isset( $wp_query->query['post_type'] ) ) {
+			$post_type = $wp_query->query['post_type'];
+		}
+	}
+
+	return $post_type;
+}
 
 /**
  * 現在ページのURLを取得
