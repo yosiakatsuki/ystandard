306d2198137dac7402d770878bf5d1afaf52cc68
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-02-26 13:10:03 +0900

    パーツコピー機能追加

--- js/admin/admin.js
+++ js/admin/admin.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";function copyClipboardForm(a){var b=jQuery(a).prev(),c=jQuery(a).next();b.select(),document.execCommand("copy"),c.length&&c.show().fadeOut(2e3)}document.addEventListener("DOMContentLoaded",function(){var a=jQuery(".copy-form__button");a.length&&a.each(function(a,b){b.addEventListener("click",function(a){a.preventDefault(),copyClipboardForm(a.target)})})});
