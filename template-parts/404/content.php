a63b2e39a25b064265744b1f72a2cc0d753ead38
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-04-11 14:56:12 +0900

    404ページ

--- template-parts/404/content.php
+++ template-parts/404/content.php
@@ -8,23 +8,24 @@
  */
 
 ?>
-<div class="content__wrap">
-	<main id="main" class="site-main content__main no-results not-found">
-		<?php do_action( 'ys_content_main_prepend' ); ?>
-		<div class="no-results__header not-found__header">
-			<h2 class="singular__title entry__title no-results__title not-found__title clear-headline">
-				<?php if ( is_search() ) : ?>
-					<?php echo esc_html( '"' . get_search_query( false ) . '"' ); ?>の検索結果が見つかりませんでした。
-				<?php else : ?>
-					お探しのページは見つかりませんでした
-				<?php endif; ?>
-			</h2>
-		</div><!-- .entry-header -->
+<main id="main" class="content__main site-main no-results not-found">
+	<?php do_action( 'ys_content_main_prepend' ); ?>
+	<div class="no-results__header not-found__header">
+		<h2 class="singular__title entry__title no-results__title not-found__title">
+			<?php if ( is_search() ) : ?>
+				<?php echo esc_html( '"' . get_search_query( false ) . '"' ); ?>の検索結果が見つかりませんでした。
+			<?php else : ?>
+				お探しのページは見つかりませんでした
+			<?php endif; ?>
+		</h2>
+	</div>
 
+	<div class="no-results__content entry-content">
 		<div class="no-results__search">
-			<h2 class="no-results__headline">サイト内を検索する</h2>
+			<p class="no-results__search-title">サイト内を検索する</p>
 			<?php get_search_form(); ?>
-		</div><!-- .no-results__search -->
-		<?php do_action( 'ys_content_main_append' ); ?>
-	</main>
-</div>
+		</div>
+		<?php do_action( 'ys_no_results_content' ); ?>
+	</div>
+	<?php do_action( 'ys_content_main_append' ); ?>
+</main>
