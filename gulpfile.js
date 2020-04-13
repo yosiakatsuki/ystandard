9bdf0d8cb38a6da1cb7ef377f5eb5cfc9b7d9851
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-04-14 00:53:17 +0900

    ブロック関連のスタイル調整

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const autoprefixer = require( 'autoprefixer' );
 const mqpacker = require( 'css-mqpacker' );
 const cssnano = require( 'cssnano' );
 const packageImporter = require( 'node-sass-package-importer' );
+const cssdeclsort = require( 'css-declaration-sorter' );
 const babel = require( 'gulp-babel' );
 const del = require( 'del' );
 const webpackStream = require( 'webpack-stream' );
@@ -18,137 +19,137 @@ const webpackConfig = require( './webpack.config' );
  * PostCssで使うプラグイン
  */
 const postcssPlugins = [
-    autoprefixer( {
-        grid: 'autoplace'
-    } ),
-    mqpacker(),
-    cssnano()
+	autoprefixer( {
+		grid: 'autoplace'
+	} ),
+	mqpacker(),
+	cssnano(),
+	cssdeclsort( { order: 'smacss' } )
 ];
 const postcssPluginsParts = [
-    autoprefixer( {
-        overrideBrowserslist: [ 'last 2 version, not ie < 11' ],
-        grid: 'autoplace'
-    } ),
-    mqpacker()
+	autoprefixer( {
+		overrideBrowserslist: [ 'last 2 version, not ie < 11' ],
+		grid: 'autoplace'
+	} ),
+	mqpacker()
 ];
 /**
  * babel option
  */
 const babelOption = {
-    presets: [ '@babel/preset-env', 'minify' ],
-    comments: false
+	presets: [ '@babel/preset-env', 'minify' ],
+	comments: false
 };
 
 /**
  * sass
  */
 function sass() {
-    return src( './src/sass/*.scss' )
-        .pipe( gulpSass( {
-            importer: packageImporter( {
-                extensions: [ '.scss', '.css' ]
-            } )
-        } ) )
-        .pipe( postcss( postcssPlugins ) )
-        .pipe( dest( './css' ) )
+	return src( './src/sass/*.scss' )
+		.pipe( gulpSass( {
+			importer: packageImporter( {
+				extensions: [ '.scss', '.css' ]
+			} )
+		} ) )
+		.pipe( postcss( postcssPlugins ) )
+		.pipe( dest( './css' ) )
 }
 
 /**
  * sass - parts
  */
 function sassParts() {
-    return src( './src/sass/inline-parts/*.scss' )
-        .pipe( gulpSass() )
-        .pipe( postcss( postcssPluginsParts ) )
-        .pipe( dest( './src/css/inline-parts' ) )
+	return src( './src/sass/inline-parts/*.scss' )
+		.pipe( gulpSass() )
+		.pipe( postcss( postcssPluginsParts ) )
+		.pipe( dest( './src/css/inline-parts' ) )
 }
 
 /**
  * JS
  */
 function js() {
-    return src( 'src/js/*.js' )
-        .pipe( babel( babelOption ) )
-        .pipe( dest( 'js/' ) )
+	return src( 'src/js/*.js' )
+		.pipe( babel( babelOption ) )
+		.pipe( dest( 'js/' ) )
 }
 
 function jsAdmin() {
-    return src( 'src/js/admin/*.js' )
-        .pipe( babel( babelOption ) )
-        .pipe( dest( 'js/admin/' ) )
+	return src( 'src/js/admin/*.js' )
+		.pipe( babel( babelOption ) )
+		.pipe( dest( 'js/admin/' ) )
 }
 
 function buildWebpack() {
-    return webpackStream( webpackConfig, webpack )
-        .pipe( dest( 'js/' ) )
+	return webpackStream( webpackConfig, webpack )
+		.pipe( dest( 'js/' ) )
 }
 
 /**
  * 必要ファイルのコピー
  */
 function copyProductionFiles() {
-    return src(
-        [
-            '**',
+	return src(
+		[
+			'**',
 			'!.github',
 			'!.github/**',
 			'!.travis',
 			'!.travis/**',
 			'!bin',
 			'!bin/**',
-            '!node_modules',
-            '!node_modules/**',
-            '!src',
-            '!src/**',
-            '!tests',
-            '!tests/**',
-            '!.csscomb.json',
-            '!.editorconfig',
-            '!.eslintrc.json',
-            '!.gitignore',
-            '!.travis.yml',
-            '!gulpfile.js',
-            '!package.json',
-            '!package-lock.json',
-            '!phpcs.ruleset.xml',
-            '!phpunit.xml.dist',
-            '!webpack.config.js',
-            '!ystandard-info.json',
-            '!ystandard-info-beta.json',
-            '!block/**/*.js',
-            '!block/**/*.scss',
-            '!build',
-            '!build/**',
-            '!*.zip',
-            '!ystandard',
-            '!ystandard/**',
-        ],
-        { base: '.' }
-    )
-        .pipe( dest( './ystandard' ) );
+			'!node_modules',
+			'!node_modules/**',
+			'!src',
+			'!src/**',
+			'!tests',
+			'!tests/**',
+			'!.editorconfig',
+			'!.eslintrc.json',
+			'!.gitignore',
+			'!.travis.yml',
+			'!gulpfile.js',
+			'!package.json',
+			'!package-lock.json',
+			'!phpcs.ruleset.xml',
+			'!phpunit.xml.dist',
+			'!webpack.config.js',
+			'!ystandard-info.json',
+			'!ystandard-info-beta.json',
+			'!block/**/*.js',
+			'!block/**/*.scss',
+			'!build',
+			'!build/**',
+			'!*.zip',
+			'!ystandard',
+			'!ystandard/**',
+		],
+		{ base: '.' }
+	)
+		.pipe( dest( './ystandard' ) );
 }
 
 /**
  * create zip file
  */
 function zip() {
-    return src( 'ystandard/**', { base: '.' } )
-        .pipe( gulpZip( 'ystandard.zip' ) )
-        .pipe( dest( 'build' ) );
+	return src( 'ystandard/**', { base: '.' } )
+		.pipe( gulpZip( 'ystandard.zip' ) )
+		.pipe( dest( 'build' ) );
 }
 
 function copyJson() {
-    return src( [ 'ystandard-info.json', 'ystandard-info-beta.json' ] )
-        .pipe( dest( 'build' ) );
+	return src( [ 'ystandard-info.json', 'ystandard-info-beta.json' ] )
+		.pipe( dest( 'build' ) );
 }
 
 function cleanFiles( cb ) {
-    return del(
-        [
-            './ystandard',
-            './build'
-        ],
-        cb );
+	return del(
+		[
+			'./ystandard',
+			'./build'
+		],
+		cb );
 }
 
 /**
@@ -169,11 +170,11 @@ exports.build = series( cleanFiles, parallel( sass, sassParts, js, jsAdmin, buil
  * default
  */
 exports.default = function () {
-    cleanFiles();
-    sass();
-    watch( [ './src/sass/**/*.scss', '!./src/sass/inline-parts/**/*.scss' ], sass );
-    watch( './src/sass/inline-parts/**/*.scss', sassParts );
-    watch( './src/js/*.js', js );
-    watch( './src/js/admin/*.js', jsAdmin );
-    watch( './src/js/**/*.js', buildWebpack );
+	cleanFiles();
+	sass();
+	watch( [ './src/sass/**/*.scss', '!./src/sass/inline-parts/**/*.scss' ], sass );
+	watch( './src/sass/inline-parts/**/*.scss', sassParts );
+	watch( './src/js/*.js', js );
+	watch( './src/js/admin/*.js', jsAdmin );
+	watch( './src/js/**/*.js', buildWebpack );
 };
