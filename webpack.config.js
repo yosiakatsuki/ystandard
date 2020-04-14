566af5e594e1913c30dfebe951540b15c8b76fc4
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-04-14 16:54:29 +0900

    polyfill調整

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
 module.exports = {
-    mode: "production",
+	mode: "production",
 
-    entry: {
-        'font-awesome-ystd': './src/js/font-awesome/app.js',
-        'polyfill': './src/js/polyfill/polyfill.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: `${ __dirname }/js`
-    },
+	entry: {
+		'font-awesome-ystd': './src/js/font-awesome/app.js',
+		'polyfill': './src/js/polyfill/polyfill.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: `${ __dirname }/js`
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								'@babel/preset-env',
+							]
+						}
+					}
+				]
+			}
+		]
+	}
 };
