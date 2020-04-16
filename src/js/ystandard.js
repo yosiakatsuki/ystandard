758d4ecc3f74c3fb03b1895bf9eba80ca93ae296
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-04-16 17:20:23 +0900

    スムーススクロール追加

--- src/js/ystandard.js
+++ src/js/ystandard.js
@@ -1,4 +1,5 @@
 document.addEventListener( 'DOMContentLoaded', () => {
+	// 検索ボタン.
 	const searchButton = document.getElementById( 'global-nav__search-button' );
 	if ( searchButton ) {
 		searchButton.addEventListener( 'click', ( e ) => {
@@ -8,8 +9,25 @@ document.addEventListener( 'DOMContentLoaded', () => {
 			}
 		} );
 	}
-	document.getElementById( 'global-nav__toggle' ).addEventListener( 'click', (e) => {
+	// メニュー.
+	document.getElementById( 'global-nav__toggle' ).addEventListener( 'click', ( e ) => {
 		e.currentTarget.classList.toggle( 'is-open' );
 	} );
-
+	// スムーススクロール.
+	const links = document.querySelectorAll( 'a[href^="#"]' );
+	for ( let i = 0; i < links.length; i++ ) {
+		links[ i ].addEventListener( 'click', ( e ) => {
+			e.preventDefault();
+			const id = e.currentTarget.getAttribute( 'href' ).replace( '#', '' );
+			const target = document.getElementById( id );
+			if ( ! target ) {
+				return;
+			}
+			const pos = target.getBoundingClientRect().top;
+			window.scroll( {
+				top: pos + window.pageYOffset - ( 16 * 3 ),
+				behavior: 'smooth'
+			} );
+		} );
+	}
 } );
