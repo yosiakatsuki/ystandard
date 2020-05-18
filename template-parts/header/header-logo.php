cf6d1ced3053eaee984dd3e35f4465da2ec90c0e
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-05-18 16:58:38 +0900

    fix #285

--- template-parts/header/header-logo.php
+++ template-parts/header/header-logo.php
@@ -15,7 +15,7 @@
 	 * ヘッダーロゴ
 	 */
 	$logo = ys_get_header_logo();
-	if ( is_front_page() || is_404() ) {
+	if ( is_front_page() || is_home() || is_404() ) {
 		printf( '<h1 class="site-title">%s</h1>', $logo );
 	} else {
 		printf( '<div class="site-title">%s</div>', $logo );
