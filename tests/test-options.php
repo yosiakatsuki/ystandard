89fe2ba348b9ff53397201aab99f3abb838b4b67
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-05-18 17:28:00 +0900

    fix #282

--- tests/test-options.php
+++ tests/test-options.php
@@ -24,20 +24,4 @@ class OptionsTest extends WP_UnitTestCase {
 		update_option( 'ys_design_header_type', 'center' );
 		$this->assertSame( ys_get_option( 'ys_design_header_type', 'row1' ), 'center' );
 	}
-
-	/**
-	 * ys_get_option_by_bool
-	 */
-	function test_ys_get_option_by_bool_true() {
-		update_option( 'ys_show_post_thumbnail', '1' );
-		$this->assertTrue( ys_get_option_by_bool( 'ys_show_post_thumbnail', false ) );
-	}
-
-	/**
-	 * ys_get_option_by_bool
-	 */
-	function test_ys_get_option_by_bool_false() {
-		update_option( 'ys_show_post_thumbnail', '' );
-		$this->assertFalse( ys_get_option_by_bool( 'ys_show_post_thumbnail' ) );
-	}
 }
