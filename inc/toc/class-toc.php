063f9ee381dba4a088d4698dac77223b5ac15ba6
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-08-31 15:35:50 +0900

    5.5 非推奨関数の対応漏れ対処

--- inc/toc/class-toc.php
+++ inc/toc/class-toc.php
@@ -224,7 +224,7 @@ class TOC {
 		for ( $i = 0; $i < count( $data ); $i ++ ) {
 			$toc .= $this->get_toc_item_html( $data, $i );
 			// 下層アイテムの作成.
-			if ( (int) $data[ $i ][2] < (int) $data[ $i + 1 ][2] ) {
+			if ( $i + 1 > count( $data ) && (int) $data[ $i ][2] < (int) $data[ $i + 1 ][2] ) {
 				$i ++;
 				$i = $this->get_toc_children( $toc, $data, $i );
 			}
@@ -254,13 +254,13 @@ class TOC {
 		for ( $i = $num; $i < count( $data ); $i ++ ) {
 			$toc .= $this->get_toc_item_html( $data, $i );
 			// 下層アイテムの作成.
-			if ( (int) $data[ $i ][2] < (int) $data[ $i + 1 ][2] ) {
+			if ( $i + 1 > count( $data ) && (int) $data[ $i ][2] < (int) $data[ $i + 1 ][2] ) {
 				$i ++;
 				$i = $this->get_toc_children( $toc, $data, $i );
 			}
 			$toc .= '</li>' . PHP_EOL;
 			// 自レベル以上のアイテムが来たら終了.
-			if ( (int) $data[ $i ][2] > (int) $data[ $i + 1 ][2] ) {
+			if ( $i + 1 > count( $data ) && (int) $data[ $i ][2] > (int) $data[ $i + 1 ][2] ) {
 				$num = $i;
 				break;
 			}
