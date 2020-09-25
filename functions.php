421be5aee2bb0d00a1e95f8a63a52b22afe034e2
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-09-25 13:59:34 +0900

    不要コード削除

--- functions.php
+++ functions.php
@@ -11,19 +11,3 @@
  * 関数群を読み込み
  */
 require_once __DIR__ . '/inc/class-ys-loader.php';
-
-
-add_filter(
-	'ys_get_archive_default_image',
-	function ( $image ) {
-		return wp_get_attachment_image(
-			6196,
-			'full',
-			false,
-			[
-				'class' => 'archive__image',
-				'alt'   => get_the_title(),
-			]
-		);
-	}
-);
