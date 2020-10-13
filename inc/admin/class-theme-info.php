e7c15d7b14be338cbdc03966eb97c523394103cf
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-10-13 22:07:01 +0900

    yStandardのお知らせ取得不具合修正

--- inc/admin/class-theme-info.php
+++ inc/admin/class-theme-info.php
@@ -22,6 +22,17 @@ class Theme_Info {
 	const RSS_URL = 'https://wp-ystandard.com/category/infomation/feed/';
 
 	/**
+	 * REST API URL
+	 */
+	const REST_POST_URL = 'http://wp-ystandard.com/wp-json/wp/v2/posts?per_page=5&categories=1';
+
+	/**
+	 * お知らせカテゴリー情報取得
+	 */
+	const REST_CATEGORY_URL = 'https://wp-ystandard.com/wp-json/wp/v2/categories?parent=1';
+
+
+	/**
 	 * お知らせ取得のキャッシュキー
 	 */
 	const RSS_CACHE_KEY = 'ystandard_info';
@@ -82,12 +93,12 @@ class Theme_Info {
 	 * @return array
 	 */
 	public function get_feed() {
-
 		if ( ! function_exists( 'fetch_feed' ) ) {
 			include_once( ABSPATH . WPINC . '/feed.php' );
 		}
 		// キャッシュ取得.
 		$cache = get_transient( self::RSS_CACHE_KEY );
+
 		if ( false !== $cache ) {
 			if ( is_array( $cache ) && ! empty( $cache ) ) {
 				if ( isset( $cache[0]['date'] ) && isset( $cache[0]['url'] ) && isset( $cache[0]['title'] ) ) {
@@ -96,14 +107,33 @@ class Theme_Info {
 			}
 		}
 
-		$rss = fetch_feed( self::RSS_URL );
-		if ( is_wp_error( $rss ) ) {
-			return [];
+		/**
+		 * RSSで取得
+		 */
+		$data = [];// $this->get_feed_from_rss();
+
+		if ( empty( $data ) ) {
+			$data = $this->get_feed_from_rest();
 		}
+		if ( ! empty( $data ) ) {
+			set_transient( self::RSS_CACHE_KEY, $data, ( 60 * 60 * 24 ) );
+		}
+
+		return $data;
+	}
+
+
+	/**
+	 * RSSから情報取得
+	 *
+	 * @return array
+	 */
+	private function get_feed_from_rss() {
+		$data      = [];
+		$rss       = fetch_feed( self::RSS_URL );
 		$maxitems  = $rss->get_item_quantity( 5 );
 		$rss_items = $rss->get_items( 0, $maxitems );
 
-		$data = [];
 		foreach ( $rss_items as $item ) {
 			$data[] = [
 				'date'  => $item->get_date( 'Y.m.d' ),
@@ -111,7 +141,38 @@ class Theme_Info {
 				'title' => wp_encode_emoji( $item->get_title() ),
 			];
 		}
-		set_transient( self::RSS_CACHE_KEY, $data, ( 60 * 60 * 24 ) );
+
+		return $data;
+	}
+
+	/**
+	 * REST APIから取得
+	 * @return array
+	 */
+	private function get_feed_from_rest() {
+
+		$child    = '';
+		$category = wp_remote_get( self::REST_CATEGORY_URL );
+		if ( ! is_wp_error( $category ) ) {
+			$category = json_decode( $category['body'] );
+			if ( is_array( $category ) ) {
+				foreach ( $category as $cat ) {
+					$child .= ',' . $cat->id;
+				}
+			}
+		}
+		$data     = [];
+		$response = wp_remote_get( self::REST_POST_URL . $child );
+		if ( ! is_wp_error( $response ) ) {
+			$body = json_decode( $response['body'] );
+			foreach ( $body as $item ) {
+				$data[] = [
+					'date'  => date_i18n( 'Y.m.d', strtotime( $item->date ) ),
+					'url'   => $item->link,
+					'title' => wp_encode_emoji( $item->title->rendered ),
+				];
+			}
+		}
 
 		return $data;
 	}
