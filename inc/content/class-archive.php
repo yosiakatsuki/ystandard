b79aee36184ccd9e105a9ad3b5ef949a7d3a67d1
yosiakatsuki <y.ogata@yosiakatsuki.net>
2020-11-24 14:03:28 +0900

    年・月・日、検索結果 アーカイブタイトル調整

--- inc/content/class-archive.php
+++ inc/content/class-archive.php
@@ -20,9 +20,97 @@ class Archive {
 	 * アクション・フィルターの登録
 	 */
 	public function register() {
+		add_filter( 'get_the_archive_title', [ $this, 'archive_title' ] );
+		add_filter( 'get_the_archive_title_prefix', '__return_empty_string' );
 		add_action( 'customize_register', [ $this, 'customize_register' ] );
 		add_filter( 'get_the_archive_description', [ $this, 'archive_description' ], 999 );
 	}
+
+	/**
+	 * アーカイブタイトル
+	 *
+	 * @param string $title title.
+	 *
+	 * @return string
+	 */
+	public function archive_title( $title ) {
+		/**
+		 * 標準フォーマット
+		 */
+		$title_format = apply_filters( 'ys_archive_title_format', '%s' );
+		/**
+		 * ページング
+		 */
+		$paged = get_query_var( 'paged' );
+
+		if ( is_category() ) {
+			$title = sprintf( $title_format, single_cat_title( '', false ) );
+		} elseif ( is_tag() ) {
+			$title = sprintf( $title_format, single_tag_title( '', false ) );
+		} elseif ( is_author() ) {
+			$title = sprintf( $title_format, get_the_author() );
+		} elseif ( is_search() ) {
+			/* translators: %s: Search Keywords. */
+			$title_format = __( 'Search Results for "%s"', 'ystandard' );
+			$title        = sprintf( $title_format, esc_html( get_search_query( false ) ) );
+		} elseif ( is_post_type_archive() ) {
+			$title = sprintf( $title_format, post_type_archive_title( '', false ) );
+		} elseif ( is_tax() ) {
+			$title = sprintf( '%s', single_term_title( '', false ) );
+		} elseif ( is_home() ) {
+			if ( 'page' === get_option( 'show_on_front' ) && get_option( 'page_for_posts' ) ) {
+				$title = get_the_title( get_option( 'page_for_posts' ) );
+			} else {
+				$title = '';
+			}
+		}
+
+		return apply_filters( 'ys_get_the_archive_title', $title, $paged );
+	}
+
+	/**
+	 * アーカイブURL
+	 */
+	public static function get_archive_url() {
+		$url            = '';
+		$queried_object = get_queried_object();
+		if ( is_category() ) {
+			$url = get_category_link( $queried_object->term_id );
+		} elseif ( is_tag() ) {
+			$url = get_tag_link( $queried_object->term_id );
+		} elseif ( is_author() ) {
+			$author_id = get_query_var( 'author' );
+			$url       = esc_url( get_author_posts_url( get_the_author_meta( 'ID', $author_id ) ) );
+		} elseif ( is_search() ) {
+			$url = home_url( '/?s=' . urlencode( get_search_query( false ) ) );
+			$url = get_post_type_archive_link( esc_url_raw( $url ) );
+		} elseif ( is_post_type_archive() ) {
+			$post_type = Content::get_post_type();
+			$url       = get_post_type_archive_link( $post_type );
+		} elseif ( is_tax() ) {
+			$tax = get_taxonomy( $queried_object->taxonomy );
+			$url = get_term_link( $queried_object->term_id, $tax->name );
+		}
+
+		return apply_filters( 'ys_get_the_archive_url', $url );
+	}
+
+	/**
+	 * アーカイブアイテムクラス作成
+	 *
+	 * @return array
+	 */
+	public static function get_archive_item_class() {
+		$classes = [];
+		/**
+		 * 共通でセットするクラス
+		 */
+		$classes[] = 'archive__item';
+		$classes[] = 'is-' . Archive::get_archive_type();
+
+		return apply_filters( 'get_archive_item_class', $classes );
+	}
+
 
 	/**
 	 * アーカイブタイプ取得
