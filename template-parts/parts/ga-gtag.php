69b3a39f153c78a6d62cf57007b29374221ee22b
yosiakatsuki <y.ogata@yosiakatsuki.net>
2021-01-13 10:58:06 +0900

    Google Analytics 追加パラーメーターカスタマイズ用フック追加

--- template-parts/parts/ga-gtag.php
+++ template-parts/parts/ga-gtag.php
@@ -10,11 +10,15 @@
 if ( ! isset( $ys_tracking_id ) || empty( $ys_tracking_id ) ) {
 	return;
 }
+$ys_ga_option = '';
+if ( ! empty( $ys_tracking_option ) ) {
+	$ys_ga_option = ' ,' . $ys_tracking_option;
+}
 ?>
 <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $ys_tracking_id; ?>"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', '<?php echo $ys_tracking_id; ?>', { 'useAmpClientId': true } );
+gtag('config', '<?php echo $ys_tracking_id; ?>'<?php echo $ys_ga_option; ?> );
 </script>
