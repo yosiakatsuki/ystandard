5ae803a43679750c542efa7c4d4797d5f23913c0
yosiakatsuki <y.ogata@yosiakatsuki.net>
2021-05-07 09:25:43 +0900

    検索フォーム内にアクションフックを追加

--- searchform.php
+++ searchform.php
@@ -10,5 +10,6 @@
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<input type="search" class="search-field" placeholder="<?php _e( 'Search' ); ?>" value="<?php echo get_search_query(); ?>" name="s" aria-label="<?php _e( 'Search' ); ?>"/>
+	<?php do_action( 'ys_search_form' ); ?>
 	<button type="submit" class="search-submit" aria-label="search"><?php echo ys_get_icon( 'search' ); ?></button>
 </form>
