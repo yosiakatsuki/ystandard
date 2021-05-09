0bfa556a6b279f087a6170bd274a3f3a893d8ab7
yosiakatsuki <y.ogata@yosiakatsuki.net>
2021-05-09 22:01:06 +0900

    fix phpcs

--- inc/icon/class-font-awesome.php
+++ inc/icon/class-font-awesome.php
@@ -36,8 +36,10 @@ class Font_Awesome {
 			add_action( 'customize_register', [ $this, 'customize_register' ] );
 			Notice::set_notice(
 				function () {
-					Notice::warning( '<p>yStandardのFont Awesome関連の設定はまもなく廃止されます。<br>アイコン表示にFont Awesome以外を使うか別プラグインでFont Awesomeのアイコンフォントを読み込むようにしてください。<br>このメッセージを消すためには「外観」→「カスタマイザー」を開き、「[ys]デザイン」→「アイコンフォント設定」で以下のように設定を変更してください。</p><ul><li>
-・アイコンフォント（Font Awesome）読み込み方式 → 読み込まない(デフォルト)</li><li>・Font Awesome Kits URL → 中身を削除（空白にする）</li></ul><p>このメッセージが表示されている間はyStandardのアップデートをしないようにしてください。</p>' );
+					Notice::warning(
+						'<p>yStandardのFont Awesome関連の設定はまもなく廃止されます。<br>アイコン表示にFont Awesome以外を使うか別プラグインでFont Awesomeのアイコンフォントを読み込むようにしてください。<br>このメッセージを消すためには「外観」→「カスタマイザー」を開き、「[ys]デザイン」→「アイコンフォント設定」で以下のように設定を変更してください。</p><ul><li>
+・アイコンフォント（Font Awesome）読み込み方式 → 読み込まない(デフォルト)</li><li>・Font Awesome Kits URL → 中身を削除（空白にする）</li></ul><p>このメッセージが表示されている間はyStandardのアップデートをしないようにしてください。</p>'
+					);
 				}
 			);
 		}
