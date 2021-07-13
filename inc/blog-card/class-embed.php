578c80b07d7940aa2ff73bd3dec08e1323a89903
yosiakatsuki <y.ogata@yosiakatsuki.net>
2021-07-13 10:42:30 +0900

    ウィジェット:レガシーウィジェット用調整

--- inc/blog-card/class-embed.php
+++ inc/blog-card/class-embed.php
@@ -30,7 +30,7 @@ class Embed {
 			remove_action( 'embed_content_meta', 'print_embed_sharing_button' );
 			remove_action( 'embed_footer', 'print_embed_sharing_dialog' );
 			add_filter(
-				Admin::BLOCK_EDITOR_ASSETS_HOOK,
+				Block_Editor_Assets::BLOCK_EDITOR_ASSETS_HOOK,
 				function ( $css ) {
 					$css .= '.block-editor__container .wp-embedded-content {height:auto;}';
 
