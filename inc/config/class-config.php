97e13c385883ce88ec34dfca4708596581ec2fe5
yosiakatsuki <y.ogata@yosiakatsuki.net>
2021-07-13 09:27:51 +0900

    Create class-config.php

---
+++ inc/config/class-config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Config
+ *
+ * @package ystandard
+ * @author  yosiakatsuki
+ * @license GPL-2.0+
+ */
+
+namespace ystandard;
+
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Class Config
+ *
+ * @package ystandard
+ */
+class Config {
+
+}
