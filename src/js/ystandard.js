77ba9b09a455d41de9ff800294a2ad28a789ca29
yosiakatsuki <y.ogata@yosiakatsuki.net>
2021-08-24 11:24:36 +0900

    固定ヘッダー時のコンテンツ域自動調整機能追加

--- src/js/ystandard.js
+++ src/js/ystandard.js
@@ -1,114 +1,173 @@
-document.addEventListener('DOMContentLoaded', () => {
-	// 検索ボタン.
-	const searchButton = document.getElementById('global-nav__search-button');
-	if (searchButton) {
-		searchButton.addEventListener('click', () => {
-			const search = document.getElementById('global-nav__search');
-			if (search) {
-				search.classList.toggle('is-active');
-				setTimeout(function () {
+/**
+ * 検索ボタンの開閉処理
+ */
+const setGlobalNavSearch = () => {
+	const searchButton = document.getElementById( 'global-nav__search-button' );
+	if ( searchButton ) {
+		searchButton.addEventListener( 'click', () => {
+			const search = document.getElementById( 'global-nav__search' );
+			if ( search ) {
+				search.classList.toggle( 'is-active' );
+				setTimeout( function () {
 					document
-						.querySelector('#global-nav__search .search-field')
+						.querySelector( '#global-nav__search .search-field' )
 						.focus();
-				}, 50);
+				}, 50 );
 			}
-		});
+		} );
 	}
-	const closeButton = document.getElementById('global-nav__search-close');
-	if (closeButton) {
-		closeButton.addEventListener('click', () => {
-			const search = document.getElementById('global-nav__search');
-			if (search) {
-				search.classList.remove('is-active');
+	const closeButton = document.getElementById( 'global-nav__search-close' );
+	if ( closeButton ) {
+		closeButton.addEventListener( 'click', () => {
+			const search = document.getElementById( 'global-nav__search' );
+			if ( search ) {
+				search.classList.remove( 'is-active' );
 			}
-		});
+		} );
 	}
-	// メニュー.
-	const globalNav = document.getElementById('global-nav__toggle');
-	if (globalNav) {
-		globalNav.addEventListener('click', (e) => {
-			e.currentTarget.classList.toggle('is-open');
+}
+
+/**
+ * グローバルナビゲーションの開閉処理
+ */
+const setGlobalNavToggle = () => {
+	const globalNav = document.getElementById( 'global-nav__toggle' );
+	if ( globalNav ) {
+		globalNav.addEventListener( 'click', ( e ) => {
+			e.currentTarget.classList.toggle( 'is-open' );
 			const mobileFooter =
-				document.getElementsByClassName('footer-mobile-nav');
-			if (mobileFooter) {
-				mobileFooter[0].classList.toggle('is-hide');
+				document.getElementsByClassName( 'footer-mobile-nav' );
+			if ( mobileFooter ) {
+				mobileFooter[ 0 ].classList.toggle( 'is-hide' );
 			}
-		});
+		} );
 	}
 	// グロナビ閉じる.
 	const globalNavLinks = document.querySelectorAll(
 		'.global-nav a[href*="#"]'
 	);
-	for (let i = 0; i < globalNavLinks.length; i++) {
-		globalNavLinks[i].addEventListener('click', () => {
-			const toggle = document.getElementById('global-nav__toggle');
-			if (toggle) {
-				toggle.classList.remove('is-open');
+	for ( let i = 0; i < globalNavLinks.length; i++ ) {
+		globalNavLinks[ i ].addEventListener( 'click', () => {
+			const toggle = document.getElementById( 'global-nav__toggle' );
+			if ( toggle ) {
+				toggle.classList.remove( 'is-open' );
 			}
 			const mobileFooter =
-				document.getElementsByClassName('footer-mobile-nav');
-			if (mobileFooter) {
-				mobileFooter[0].classList.remove('is-hide');
+				document.getElementsByClassName( 'footer-mobile-nav' );
+			if ( mobileFooter ) {
+				mobileFooter[ 0 ].classList.remove( 'is-hide' );
 			}
-		});
+		} );
 	}
-	// スムーススクロール.
-	const links = document.querySelectorAll('a[href^="#"]');
-	for (let i = 0; i < links.length; i++) {
-		links[i].addEventListener('click', (e) => {
+}
+
+/**
+ * ページ内リンクのスムーススクロール.
+ */
+const setSmoothScroll = () => {
+	const links = document.querySelectorAll( 'a[href^="#"]' );
+	for ( let i = 0; i < links.length; i++ ) {
+		links[ i ].addEventListener( 'click', ( e ) => {
 			e.preventDefault();
 			let top = 0;
-			const id = e.currentTarget.getAttribute('href').replace('#', '');
-			const target = document.getElementById(id);
-			if (!target && '' !== id) {
+			const id = e.currentTarget.getAttribute( 'href' ).replace( '#', '' );
+			const target = document.getElementById( id );
+			if ( ! target && '' !== id ) {
 				return;
 			}
-			if (target) {
+			if ( target ) {
 				const pos = target.getBoundingClientRect().top;
 				let buffer = 50;
-				const header = document.getElementById('masthead');
+				const header = document.getElementById( 'masthead' );
 				if (
 					'fixed' ===
 					window
-						.getComputedStyle(header, null)
-						.getPropertyValue('position')
+						.getComputedStyle( header, null )
+						.getPropertyValue( 'position' )
 				) {
 					buffer = header.getBoundingClientRect().bottom + 20;
 				}
 				top = pos + window.pageYOffset - buffer;
 			}
 
-			window.scroll({
+			window.scroll( {
 				top,
 				behavior: 'smooth',
-			});
-		});
+			} );
+		} );
 	}
-	// TOPへ戻る.
-	const backToTop = document.getElementById('back-to-top');
-	if (backToTop && backToTop.classList.contains('is-square')) {
+}
+
+/**
+ * TOPへ戻る.
+ */
+const setBackToTop = () => {
+	const backToTop = document.getElementById( 'back-to-top' );
+	if ( backToTop && backToTop.classList.contains( 'is-square' ) ) {
 		const width = backToTop.getBoundingClientRect().width;
 		const height = backToTop.getBoundingClientRect().height;
-		const size = width < height ? `${height}px` : `${width}px`;
+		const size = width < height ? `${ height }px` : `${ width }px`;
 		backToTop.style.width = size;
 		backToTop.style.height = size;
-		backToTop.addEventListener('click', (e) => {
+		backToTop.addEventListener( 'click', ( e ) => {
 			e.preventDefault();
-			window.scroll({
+			window.scroll( {
 				top: 0,
 				behavior: 'smooth',
-			});
-		});
+			} );
+		} );
 	}
-	// スクロールバー分.
+}
+
+/**
+ * スクロールバー幅の変数セット.
+ */
+const setScrollBarWidth = () => {
 	const scrollbar = window.innerWidth - document.body.clientWidth;
 	if (
 		window
-			.getComputedStyle(document.documentElement)
-			.getPropertyValue('--scrollbar-width')
+			.getComputedStyle( document.documentElement )
+			.getPropertyValue( '--scrollbar-width' )
 	) {
 		document
-			.querySelector(':root')
-			.style.setProperty('--scrollbar-width', scrollbar + 'px');
+			.querySelector( ':root' )
+			.style.setProperty( '--scrollbar-width', scrollbar + 'px' );
 	}
-});
+}
+
+const setFixedHeaderPadding = () => {
+	const body = document.getElementsByTagName( 'body' )[ 0 ];
+	if ( body.classList.contains( 'has-fixed-header' ) && ! body.classList.contains( 'disable-auto-padding' ) ) {
+		const header = document.getElementById( 'masthead' );
+		if ( header ) {
+			const size = Math.floor( header.getBoundingClientRect().height );
+			body.style.paddingTop = `${ size }px`;
+		}
+	}
+}
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	// スクロールバー分.
+	setScrollBarWidth();
+	// 固定ヘッダー高さセット.
+	setFixedHeaderPadding();
+	// メニュー.
+	setGlobalNavToggle();
+	// 検索ボタン.
+	setGlobalNavSearch();
+	// スムーススクロール.
+	setSmoothScroll();
+	// TOPへ戻る.
+	setBackToTop();
+} );
+
+window.addEventListener( 'resize', () => {
+	if ( window.ysResizeFixedHeader ) {
+		clearTimeout( window.ysResizeFixedHeader );
+	}
+	window.ysResizeFixedHeader = setTimeout(
+		function () {
+			// 固定ヘッダー高さセット.
+			setFixedHeaderPadding()
+		}, 100 );
+} );
