ffb6971649e29342b5b14aff34d58ee6e2cffce2
yosiakatsuki <y.ogata@yosiakatsuki.net>
2021-12-14 22:27:03 +0900

    グローバルメニューの開閉設定追加

--- inc/menu/class-global-nav.php
+++ inc/menu/class-global-nav.php
@@ -9,6 +9,8 @@
 
 namespace ystandard;
 
+use ystandard\helper\Filesystem;
+
 defined( 'ABSPATH' ) || die();
 
 /**
@@ -17,6 +19,23 @@ defined( 'ABSPATH' ) || die();
  * @package ystandard
  */
 class Global_Nav {
+
+	/**
+	 * メニュー展開サイズ.
+	 */
+	const GLOBAL_NAV_EXPAND = 768;
+	/**
+	 * メニュー展開サイズ取得用フィルター.
+	 */
+	const GLOBAL_NAV_EXPAND_FILTER = 'ys_get_global_nav_expand';
+
+
+	public function __construct() {
+		add_action( 'customize_register', [ $this, 'customize_register' ] );
+		add_filter( Enqueue_Utility::FILTER_INLINE_CSS, [ $this, 'global_nav_inline_css' ] );
+		add_filter( Enqueue_Utility::FILTER_CSS_VARS, [ $this, 'global_nav_css_vars' ] );
+		add_filter( self::GLOBAL_NAV_EXPAND_FILTER, [ __CLASS__, 'get_global_nav_expand' ] );
+	}
 
 	/**
 	 * グローバルナビゲーションクラス
@@ -60,5 +79,168 @@ class Global_Nav {
 
 		return apply_filters( 'ys_global_nav_walker', $result );
 	}
+
+	/**
+	 * カスタムプロパティ指定.
+	 *
+	 * @param array $css_vars カスタムプロパティリスト.
+	 *
+	 * @return array
+	 */
+	public function global_nav_css_vars( $css_vars ) {
+
+		$toggle_top = Option::get_option_by_int( 'ys_global_nav_toggle_top', 0 );
+		if ( 0 !== $toggle_top ) {
+			$css_vars['mobile-nav-toggle-top'] = "${toggle_top}px";
+		}
+		$css_vars['global-nav-bold'] = Option::get_option(
+			'ys_global_nav_bold',
+			'normal'
+		);
+
+		return $css_vars;
+	}
+
+	/**
+	 * 開閉式グローバルナビゲーション用
+	 *
+	 * @param string $css CSS.
+	 *
+	 * @return string
+	 */
+	public function global_nav_inline_css( $css ) {
+
+		$global_nav_css = Filesystem::file_get_contents(
+			get_template_directory() . '/css/global-navigation-fold.css'
+		);
+		$breakpoint     = apply_filters(
+			self::GLOBAL_NAV_EXPAND_FILTER,
+			self::GLOBAL_NAV_EXPAND
+		);
+		if ( Option::get_option_by_bool( 'ys_global_nav_expand_none', false ) ) {
+			$breakpoint = 9999;
+		}
+
+		$css .= str_replace(
+			'{{mobaile-nav-breakpoint}}',
+			$breakpoint . 'px',
+			$global_nav_css
+		);
+
+		return $css;
+	}
+
+	/**
+	 * メニュー展開サイズ取得
+	 *
+	 * @return int
+	 */
+	public static function get_global_nav_expand() {
+		return Option::get_option_by_int( 'ys_global_nav_expand', self::GLOBAL_NAV_EXPAND );
+	}
+
+	/**
+	 * メニュー設定
+	 *
+	 * @param \WP_Customize_Manager $wp_customize カスタマイザー.
+	 */
+	public function customize_register( $wp_customize ) {
+		$customizer = new Customize_Control( $wp_customize );
+		/**
+		 * セクション追加
+		 */
+		$customizer->add_section(
+			[
+				'section'     => 'ys_customizer_section_global_nav',
+				'title'       => 'グローバルナビゲーション',
+				'description' => 'グローバルナビゲーション設定' . Admin::manual_link( 'manual/global-nav' ),
+				'priority'    => 60,
+				'panel'       => Design::PANEL_NAME,
+			]
+		);
+		$customizer->add_section_label( 'グローバルメニューデザイン' );
+		$customizer->add_number(
+			[
+				'id'          => 'ys_header_nav_margin',
+				'default'     => 1.5,
+				'label'       => 'メニュー項目の間隔',
+				'description' => '単位はemです。大きくすればメニュー間の余白が大きくなります。',
+				'input_attrs' => [
+					'min'  => 0.1,
+					'max'  => 10.0,
+					'step' => 0.1,
+				],
+			]
+		);
+		$customizer->add_select(
+			[
+				'id'          => 'ys_global_nav_bold',
+				'default'     => 'normal',
+				'label'       => 'メニュー文字太さ',
+				'description' => 'トップレベルのメニュー文字太さ設定。',
+				'choices'     => [
+					'normal' => __( '通常(normal)', 'ystandard' ),
+					'bold'   => __( '太字(bold)', 'ystandard' ),
+					'100'    => __( '100', 'ystandard' ),
+					'200'    => __( '200', 'ystandard' ),
+					'300'    => __( '300', 'ystandard' ),
+					'400'    => __( '400', 'ystandard' ),
+					'500'    => __( '500', 'ystandard' ),
+					'600'    => __( '600', 'ystandard' ),
+					'700'    => __( '700', 'ystandard' ),
+					'800'    => __( '800', 'ystandard' ),
+					'900'    => __( '900', 'ystandard' ),
+				],
+			]
+		);
+		$customizer->add_section_label(
+			'ドロワーメニュー設定',
+			[
+				'description' => 'グローバルメニューの折りたたみ表示に関する設定',
+			]
+		);
+		$customizer->add_number(
+			[
+				'id'          => 'ys_global_nav_expand',
+				'default'     => self::GLOBAL_NAV_EXPAND,
+				'label'       => 'ドロワーメニュー開始サイズ(px)',
+				'description' => '設定した画面サイズ以下でドロワーメニュー表示になります。',
+				'input_attrs' => [
+					'min'  => 600,
+					'max'  => 1440,
+					'step' => 1,
+				],
+			]
+		);
+		$customizer->add_label(
+			[
+				'id'          => 'ys_global_nav_expand_none_label',
+				'label'       => '常にドロワーメニュー表示にする',
+				'description' => 'チェックをつけるとグローバルメニューが常にドロワーメニューになります。',
+			]
+		);
+		$customizer->add_checkbox(
+			[
+				'id'      => 'ys_global_nav_expand_none',
+				'label'   => '常にドロワーメニュー表示にする',
+				'default' => 0,
+			]
+		);
+		$customizer->add_number(
+			[
+				'id'          => 'ys_global_nav_toggle_top',
+				'default'     => 0,
+				'label'       => 'メニュー開閉ボタンの縦位置調整(px)',
+				'description' => 'メニュー開閉ボタンの上下位置を微調整できます。',
+
+				'input_attrs' => [
+					'min'  => - 100,
+					'max'  => 100,
+					'step' => 1,
+				],
+			]
+		);
+	}
 }
 
+new Global_Nav();
--- src/js/ystandard.js
+++ src/js/ystandard.js
@@ -2,28 +2,28 @@
  * 検索ボタンの開閉処理
  */
 const setGlobalNavSearch = () => {
-	const searchButton = document.getElementById('global-nav__search-button');
-	if (searchButton) {
-		searchButton.addEventListener('click', () => {
-			const search = document.getElementById('global-nav__search');
-			if (search) {
-				search.classList.toggle('is-active');
-				setTimeout(function () {
+	const searchButton = document.getElementById( 'global-nav__search-button' );
+	if ( searchButton ) {
+		searchButton.addEventListener( 'click', () => {
+			const search = document.getElementById( 'global-nav__search' );
+			if ( search ) {
+				search.classList.toggle( 'is-active' );
+				setTimeout( function () {
 					document
-						.querySelector('#global-nav__search .search-field')
+						.querySelector( '#global-nav__search .search-field' )
 						.focus();
-				}, 50);
+				}, 50 );
 			}
-		});
+		} );
 	}
-	const closeButton = document.getElementById('global-nav__search-close');
-	if (closeButton) {
-		closeButton.addEventListener('click', () => {
-			const search = document.getElementById('global-nav__search');
-			if (search) {
-				search.classList.remove('is-active');
+	const closeButton = document.getElementById( 'global-nav__search-close' );
+	if ( closeButton ) {
+		closeButton.addEventListener( 'click', () => {
+			const search = document.getElementById( 'global-nav__search' );
+			if ( search ) {
+				search.classList.remove( 'is-active' );
 			}
-		});
+		} );
 	}
 };
 
@@ -31,33 +31,33 @@ const setGlobalNavSearch = () => {
  * グローバルナビゲーションの開閉処理
  */
 const setGlobalNavToggle = () => {
-	const globalNav = document.getElementById('global-nav__toggle');
-	if (globalNav) {
-		globalNav.addEventListener('click', (e) => {
-			e.currentTarget.classList.toggle('is-open');
+	const globalNav = document.getElementById( 'global-nav__toggle' );
+	if ( globalNav ) {
+		globalNav.addEventListener( 'click', ( e ) => {
+			e.currentTarget.classList.toggle( 'is-open' );
 			const mobileFooter =
-				document.getElementsByClassName('footer-mobile-nav');
-			if (mobileFooter && mobileFooter.length) {
-				mobileFooter[0].classList.toggle('is-hide');
+				document.getElementsByClassName( 'footer-mobile-nav' );
+			if ( mobileFooter && mobileFooter.length ) {
+				mobileFooter[ 0 ].classList.toggle( 'is-hide' );
 			}
-		});
+		} );
 	}
 	// グロナビ閉じる.
 	const globalNavLinks = document.querySelectorAll(
 		'.global-nav a[href*="#"]'
 	);
-	for (let i = 0; i < globalNavLinks.length; i++) {
-		globalNavLinks[i].addEventListener('click', () => {
-			const toggle = document.getElementById('global-nav__toggle');
-			if (toggle) {
-				toggle.classList.remove('is-open');
+	for ( let i = 0; i < globalNavLinks.length; i++ ) {
+		globalNavLinks[ i ].addEventListener( 'click', () => {
+			const toggle = document.getElementById( 'global-nav__toggle' );
+			if ( toggle ) {
+				toggle.classList.remove( 'is-open' );
 			}
 			const mobileFooter =
-				document.getElementsByClassName('footer-mobile-nav');
-			if (mobileFooter) {
-				mobileFooter[0].classList.remove('is-hide');
+				document.getElementsByClassName( 'footer-mobile-nav' );
+			if ( mobileFooter ) {
+				mobileFooter[ 0 ].classList.remove( 'is-hide' );
 			}
-		});
+		} );
 	}
 };
 
@@ -65,36 +65,36 @@ const setGlobalNavToggle = () => {
  * ページ内リンクのスムーススクロール.
  */
 const setSmoothScroll = () => {
-	const links = document.querySelectorAll('a[href^="#"]');
-	for (let i = 0; i < links.length; i++) {
-		links[i].addEventListener('click', (e) => {
+	const links = document.querySelectorAll( 'a[href^="#"]' );
+	for ( let i = 0; i < links.length; i++ ) {
+		links[ i ].addEventListener( 'click', ( e ) => {
 			e.preventDefault();
 			let top = 0;
-			const id = e.currentTarget.getAttribute('href').replace('#', '');
-			const target = document.getElementById(id);
-			if (!target && '' !== id) {
+			const id = e.currentTarget.getAttribute( 'href' ).replace( '#', '' );
+			const target = document.getElementById( id );
+			if ( ! target && '' !== id ) {
 				return;
 			}
-			if (target) {
+			if ( target ) {
 				const pos = target.getBoundingClientRect().top;
 				let buffer = 50;
-				const header = document.getElementById('masthead');
+				const header = document.getElementById( 'masthead' );
 				if (
 					'fixed' ===
 					window
-						.getComputedStyle(header, null)
-						.getPropertyValue('position')
+						.getComputedStyle( header, null )
+						.getPropertyValue( 'position' )
 				) {
 					buffer = header.getBoundingClientRect().bottom + 20;
 				}
 				top = pos + window.pageYOffset - buffer;
 			}
 
-			window.scroll({
+			window.scroll( {
 				top,
 				behavior: 'smooth',
-			});
-		});
+			} );
+		} );
 	}
 };
 
@@ -102,20 +102,20 @@ const setSmoothScroll = () => {
  * TOPへ戻る.
  */
 const setBackToTop = () => {
-	const backToTop = document.getElementById('back-to-top');
-	if (backToTop && backToTop.classList.contains('is-square')) {
+	const backToTop = document.getElementById( 'back-to-top' );
+	if ( backToTop && backToTop.classList.contains( 'is-square' ) ) {
 		const width = backToTop.getBoundingClientRect().width;
 		const height = backToTop.getBoundingClientRect().height;
-		const size = width < height ? `${height}px` : `${width}px`;
+		const size = width < height ? `${ height }px` : `${ width }px`;
 		backToTop.style.width = size;
 		backToTop.style.height = size;
-		backToTop.addEventListener('click', (e) => {
+		backToTop.addEventListener( 'click', ( e ) => {
 			e.preventDefault();
-			window.scroll({
+			window.scroll( {
 				top: 0,
 				behavior: 'smooth',
-			});
-		});
+			} );
+		} );
 	}
 };
 
@@ -126,54 +126,71 @@ const setScrollBarWidth = () => {
 	const scrollbar = window.innerWidth - document.body.clientWidth;
 	if (
 		window
-			.getComputedStyle(document.documentElement)
-			.getPropertyValue('--scrollbar-width')
+			.getComputedStyle( document.documentElement )
+			.getPropertyValue( '--scrollbar-width' )
 	) {
 		document
-			.querySelector(':root')
-			.style.setProperty('--scrollbar-width', scrollbar + 'px');
+			.querySelector( ':root' )
+			.style.setProperty( '--scrollbar-width', scrollbar + 'px' );
 	}
 };
+
+const getHeaderHeight = () => {
+	const header = document.getElementById( 'masthead' );
+	if ( ! header ) {
+		return undefined;
+	}
+	return Math.floor( header.getBoundingClientRect().height );
+}
 
 const setFixedHeaderPadding = () => {
 	const classes = document.body.classList;
 	if (
-		classes.contains('has-fixed-header') &&
-		!classes.contains('disable-auto-padding') &&
-		!classes.contains('is-overlay')
+		classes.contains( 'has-fixed-header' ) &&
+		! classes.contains( 'disable-auto-padding' ) &&
+		! classes.contains( 'is-overlay' )
 	) {
-		const header = document.getElementById('masthead');
-		if (header) {
-			const size = Math.floor(header.getBoundingClientRect().height);
-			document.body.style.paddingTop = `${size}px`;
+		const size = getHeaderHeight();
+		if ( size ) {
+			document.body.style.paddingTop = `${ size }px`;
 			document
-				.querySelector(':root')
-				.style.setProperty('--fixed-sidebar-top', `${size + 48}px`);
+				.querySelector( ':root' )
+				.style.setProperty( '--fixed-sidebar-top', `${ size + 48 }px` );
 		}
 	}
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+const setDrawerNavPadding = () => {
+	const size = getHeaderHeight();
+	if ( size ) {
+		document
+			.querySelector( ':root' )
+			.style.setProperty( '--mobile-nav-container-padding', `${ size * 1.5 }px` );
+	}
+}
+
+document.addEventListener( 'DOMContentLoaded', () => {
 	// スクロールバー分.
 	setScrollBarWidth();
 	// 固定ヘッダー高さセット.
 	setFixedHeaderPadding();
 	// メニュー.
 	setGlobalNavToggle();
+	setDrawerNavPadding();
 	// 検索ボタン.
 	setGlobalNavSearch();
 	// スムーススクロール.
 	setSmoothScroll();
 	// TOPへ戻る.
 	setBackToTop();
-});
+} );
 
-window.addEventListener('resize', () => {
-	if (window.ysResizeFixedHeader) {
-		clearTimeout(window.ysResizeFixedHeader);
+window.addEventListener( 'resize', () => {
+	if ( window.ysResizeFixedHeader ) {
+		clearTimeout( window.ysResizeFixedHeader );
 	}
-	window.ysResizeFixedHeader = setTimeout(function () {
+	window.ysResizeFixedHeader = setTimeout( function () {
 		// 固定ヘッダー高さセット.
 		setFixedHeaderPadding();
-	}, 100);
-});
+	}, 100 );
+} );
