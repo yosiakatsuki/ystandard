e46fe3927565240f8b0d5a6c50068c17ee961a66
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2022-05-22 11:47:52 +0900

    画像アップロード機能調整等

---
+++ inc/helper/class-boolean.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Helper : Boolean
+ *
+ * @package ystandard
+ * @author  yosiakatsuki
+ * @license GPL-2.0+
+ */
+
+namespace ystandard\helper;
+
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Class : Boolean
+ */
+class Boolean {
+
+	/**
+	 * Boolに変換
+	 *
+	 * @param mixed $value 変換する値.
+	 *
+	 * @return bool
+	 */
+	public static function to_bool( $value ) {
+		if ( true === $value || 'true' === $value || 1 === $value || '1' === $value ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
