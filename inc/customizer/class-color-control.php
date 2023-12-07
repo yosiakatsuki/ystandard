6c5743665ba221357ec72b5ef9da1c5e9df55161
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-12-07 09:10:47 +0900

    カスタマイザー カスタムカラーコントロールのJSをインライン化

--- inc/customizer/class-color-control.php
+++ inc/customizer/class-color-control.php
@@ -38,14 +38,14 @@ if ( class_exists( 'WP_Customize_Color_Control' ) ) {
 		 */
 		public function enqueue() {
 			parent::enqueue();
-			$file_name = 'customizer-control-ys-color-control.js';
-			wp_enqueue_script(
-				'customizer-control-ys-color-control',
-				get_template_directory_uri() . "/js/admin/{$file_name}",
-				[ 'customize-controls', 'jquery', 'customize-base', 'wp-color-picker' ],
-				filemtime( get_template_directory() . "/js/admin/{$file_name}" ),
-				false
+			$handle = 'customizer-control-ys-color-control';
+			wp_register_script(
+				$handle,
+				false,
+				[ 'customize-controls', 'jquery', 'customize-base', 'wp-color-picker' ]
 			);
+			wp_add_inline_script( $handle, $this->get_script() );
+			wp_enqueue_script( $handle );
 		}
 
 		/**
@@ -54,6 +54,66 @@ if ( class_exists( 'WP_Customize_Color_Control' ) ) {
 		public function to_json() {
 			parent::to_json();
 			$this->json['palette'] = $this->palette;
+		}
+
+		private function get_script() {
+			return <<<SCRIPT
+			wp.customize.controlConstructor[ 'ys-color-control' ] = wp.customize.Control.extend( {
+				ready: function () {
+					const control = this;
+					let	updating = false;
+					const picker = this.container.find( '.color-picker-hex' );
+
+					picker.val( control.setting() ).wpColorPicker( {
+						palettes: control.params.palette,
+						change: function () {
+							updating = true;
+							control.setting.set( picker.wpColorPicker( 'color' ) );
+							updating = false;
+						},
+						clear: function () {
+							updating = true;
+							control.setting.set( '' );
+							updating = false;
+						}
+					} );
+
+					control.setting.bind( function ( value ) {
+						if ( updating ) {
+							return;
+						}
+						picker.val( value );
+						picker.wpColorPicker( 'color', value );
+					} );
+
+					control.container.on( 'keydown', function ( event ) {
+						var pickerContainer;
+						if ( 27 !== event.which ) {
+							return;
+						}
+						pickerContainer = control.container.find( '.wp-picker-container' );
+						if ( pickerContainer.hasClass( 'wp-picker-active' ) ) {
+							picker.wpColorPicker( 'close' );
+							control.container.find( '.wp-color-result' ).focus();
+							event.stopPropagation();
+						}
+					} );
+				}
+			} );
+			wp.customize.bind( 'ready', function () {
+				const $ = jQuery;
+				$( '.customize-control-ys-color-control .wp-color-result' ).each( function ( index, element ) {
+					$( element ).on( 'click', function () {
+						if ( $( this ).hasClass( 'wp-picker-open' ) ) {
+							const holder = $( this ).nextAll( '.wp-picker-holder' );
+							const pickerInner = holder.find( '.iris-picker-inner' );
+							const square = holder.find( '.iris-square' );
+							square.height( pickerInner.height() );
+						}
+					} );
+				} );
+			} );
+			SCRIPT;
 		}
 	}
 }
--- src/scripts/admin/ystandard-customizer.ts
+++ src/scripts/admin/ystandard-customizer.ts
@@ -1,8 +1,5 @@
 import {showHeaderHeightInfo} from "@ystd/admin/customizer/preview";
 import {setBackgroundPreset} from "@ystd/admin/customizer/background";
-import {extendColorControl, setColorControlClickEvent} from "@ystd/admin/customizer/color-control";
 
 showHeaderHeightInfo();
 setBackgroundPreset();
-setColorControlClickEvent();
-extendColorControl();
