6e6fbf52d625f36a80e3561b26c8eb8acc22d746
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-03-27 00:01:40 +0900

    投稿ヘッダー　0件のときの処理

--- template-parts/singular/footer.php
+++ template-parts/singular/footer.php
@@ -12,33 +12,30 @@ defined( 'ABSPATH' ) || die();
 if ( ! ys_is_active_post_footer() ) {
 	return;
 }
-?>
-<footer class="singular__footer entry-footer">
-	<?php
-	/**
-	 * 記事フッター
-	 *
-	 * 1. 記事下ウィジェット(10)
-	 *      \ystandard\Widget::singular_footer_widget
-	 * 2. 広告 (20)
-	 *      \ystandard\Advertisement::footer_ad
-	 * 3. SNSシェアボタン (30)
-	 *      template-parts/parts/sns-share-button
-	 *      \ystandard\Share_Button::footer_share_button
-	 * 4. カテゴリー・タグ (40)
-	 *      template-parts/parts/post-taxonomy
-	 *      \ystandard\Taxonomy::post_taxonomy
-	 * 5. 著者(50)
-	 *      template-parts/parts/author
-	 *      \ystandard\Author::post_author
-	 * 6. 関連記事(60) parts/post-related
-	 *      \ystandard\Content::related_posts
-	 * 7. コメント(70)
-	 *      \ystandard\Comment::post_comment
-	 * 8. 前の記事・次の記事(80)
-	 *      template-parts/parts/post-paging
-	 *      \ystandard\Paging::post_paging
-	 */
-	do_action( 'ys_singular_footer' );
-	?>
-</footer><!-- .entry__footer -->
+/**
+ * 記事フッター
+ *
+ * 1. 記事下ウィジェット(10)
+ *      \ystandard\Widget::singular_footer_widget
+ * 2. 広告 (20)
+ *      \ystandard\Advertisement::footer_ad
+ * 3. SNSシェアボタン (30)
+ *      template-parts/parts/sns-share-button
+ *      \ystandard\Share_Button::footer_share_button
+ * 4. カテゴリー・タグ (40)
+ *      template-parts/parts/post-taxonomy
+ *      \ystandard\Taxonomy::post_taxonomy
+ * 5. 著者(50)
+ *      template-parts/parts/author
+ *      \ystandard\Author::post_author
+ * 6. 関連記事(60) parts/post-related
+ *      \ystandard\Content::related_posts
+ * 7. コメント(70)
+ *      \ystandard\Comment::post_comment
+ * 8. 前の記事・次の記事(80)
+ *      template-parts/parts/post-paging
+ *      \ystandard\Paging::post_paging
+ */
+echo '<footer class="singular__footer entry-footer">';
+do_action( 'ys_singular_footer' );
+echo '</footer>';
