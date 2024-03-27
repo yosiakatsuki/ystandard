db428018efb27b7b4b8c189e7e1975319f8ea551
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-03-27 14:58:16 +0900

    block-styles.jsonからブロックスタイルを追加できる機能追加

--- inc/blocks/class-block-styles.php
+++ inc/blocks/class-block-styles.php
@@ -31,14 +31,24 @@ class Block_Styles {
 		if ( ! function_exists( 'register_block_style' ) ) {
 			return;
 		}
-		$theme  = $this->get_theme_block_style();
+		// テーマで追加したスタイル.
+		$theme = $this->get_theme_block_style();
+		// JSONで追加したスタイル,
+		$from_json = $this->get_block_style_from_json();
+		if ( ! is_array( $from_json ) ) {
+			$custom = [];
+		}
+		// フックで追加したスタイル.
 		$custom = apply_filters( 'ys_register_block_styles', [] );
 		if ( ! is_array( $custom ) ) {
 			$custom = [];
 		}
-		$block_styles = array_merge( $theme, $custom );
+		// ブロックスタイルをマージ.
+		$block_styles = array_merge( $theme, $from_json, $custom );
 		foreach ( $block_styles as $item ) {
-			register_block_style( $item['block_name'], $item );
+			if ( isset( $item['block_name'] ) ) {
+				register_block_style( $item['block_name'], $item );
+			}
 		}
 	}
 
@@ -55,6 +65,39 @@ class Block_Styles {
 				'label'      => __( 'モバイルで1列', 'ystandard' ),
 			],
 		];
+	}
+
+	/**
+	 * JSONファイルからブロックスタイルを取得する
+	 *
+	 * @return array
+	 */
+	private function get_block_style_from_json() {
+
+		// ファイルパス.
+		$file_path = apply_filters( 'ys_block_style_json_file_path', '/block-styles.json' );
+
+		// テーマ・子テーマ内のパスを取得.
+		$path = get_theme_file_path( $file_path );
+
+		if ( ! file_exists( $path ) ) {
+			return [];
+		}
+
+		// JSONファイルを配列形式で読み込む.
+		$block_styles = wp_json_file_decode( $path, [ 'associative' => true ] );
+		if ( empty( $block_styles ) ) {
+			return [];
+		}
+		$result = [];
+		// ブロックスタイルの形式が正しいかチェック.
+		foreach ( $block_styles as $item ) {
+			if ( isset( $item['block_name'] ) && isset( $item['name'] ) && isset( $item['label'] ) ) {
+				$result[] = $item;
+			}
+		}
+
+		return $result;
 	}
 }
 
