5cfccdb778c84aa8354d8cc8c97e83492d4fea34
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-04-10 15:41:55 +0900

    投稿ヘッダー・投稿フッター分割

---
+++ inc/post/class-post-footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 投稿フッター
+ *
+ * @package ystandard
+ * @author  yosiakatsuki
+ * @license GPL-3.0+
+ */
+
+namespace ystandard;
+
+use ystandard\utils\Post_Type;
+
+class Post_Footer {
+
+	/**
+	 * 投稿フッターを表示するか
+	 *
+	 * @return bool
+	 */
+	public static function is_active_post_footer() {
+		$result = true;
+		if ( is_singular() ) {
+			// 投稿タイプ別フック.
+			$post_type = Post_Type::get_post_type();
+			$result    = apply_filters(
+				"ys_is_active_post_footer_{$post_type}",
+				$result
+			);
+		}
+
+		return apply_filters( 'ys_is_active_post_footer', $result );
+	}
+}
