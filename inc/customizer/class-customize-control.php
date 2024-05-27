a705e980e13144a53011573d4ed548c9ca96a2c6
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-05-27 17:20:51 +0900

    カスタマイザークラス改良

--- inc/customizer/class-customize-control.php
+++ inc/customizer/class-customize-control.php
@@ -9,7 +9,6 @@
 
 namespace ystandard;
 
-use ystandard\helper\Trigger_Error;
 use ystandard\utils\Convert;
 
 defined( 'ABSPATH' ) || die();
@@ -52,75 +51,166 @@ class Customize_Control {
 	}
 
 	/**
-	 * パネル追加
+	 * ラベル.
 	 *
-	 * @param array $args パラメーター.
+	 * @param array $args オプション.
 	 */
-	public function add_panel( $args ) {
-		if ( isset( $args['panel'] ) ) {
-			$args = wp_parse_args(
-				$args,
-				[
-					'title'           => '',
-					'priority'        => Customizer::get_priority( $args['panel'] ),
-					'description'     => '',
-					'active_callback' => '',
-				]
+	public function add_label( $args ) {
+		$args['control_type'] = 'hidden';
+		$args['transport']    = 'postMessage';
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * URL.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_url( $args ) {
+		$args['control_type']      = 'url';
+		$args['sanitize_callback'] = 'esc_url_raw';
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * テキストエリア.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_textarea( $args ) {
+		$args['control_type'] = 'textarea';
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * プレーンテキスト.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_plain_textarea( $args ) {
+		// サニタイズのセット.
+		$args['sanitize_callback'] = [ __CLASS__, 'sanitize_plain_text' ];
+		$this->add_textarea( $args );
+	}
+
+	/**
+	 * 数値.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_number( $args ) {
+		$args['control_type']      = 'number';
+		$args['sanitize_callback'] = [ __CLASS__, 'sanitize_number' ];
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * テキスト.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_text( $args ) {
+		$args['control_type'] = 'text';
+		if ( ! isset( $args['sanitize_callback'] ) ) {
+			$args['sanitize_callback'] = 'sanitize_text_field';
+		}
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * チェックボックス.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_checkbox( $args ) {
+		$args['control_type']      = 'checkbox';
+		$args['sanitize_callback'] = [ __CLASS__, 'sanitize_checkbox' ];
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * セレクト.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_select( $args ) {
+		$args['control_type']      = 'select';
+		$args['sanitize_callback'] = [ __CLASS__, 'sanitize_select' ];
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * ラジオボタン.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_radio( $args ) {
+		$args['control_type']      = 'radio';
+		$args['sanitize_callback'] = [ __CLASS__, 'sanitize_select' ];
+
+		$this->add_setting( $args );
+	}
+
+	/**
+	 * 画像ラジオボタン.
+	 *
+	 * @param array $args オプション.
+	 */
+	public function add_image_label_radio( $args ) {
+		$args                      = $this->parse_args( $args );
+		$args['control_type']      = 'radio';
+		$args['sanitize_callback'] = [ __CLASS__, 'sanitize_select' ];
+		$this->add_setting( $args, false );
+		if ( class_exists( __NAMESPACE__ . '\Image_Label_Radio_Control' ) ) {
+			$this->wp_customize->add_control(
+				new Image_Label_Radio_Control(
+					$this->wp_customize,
+					$args['id'],
+					self::get_control_args( $args, $args['id'] )
+				)
 			);
-			$this->wp_customize->add_panel(
-				$args['panel'],
-				$args
-			);
-			/**
-			 * パネル情報をセット
-			 */
-			$this->panel = $args['panel'];
+			$this->do_action_after_add_setting( $args['id'], $args );
 		}
 	}
 
 	/**
-	 * セクション追加
+	 * 画像.
 	 *
-	 * @param array $args パラメーター.
+	 * @param array $args オプション.
 	 */
-	public function add_section( $args ) {
-		if ( isset( $args['section'] ) ) {
-			$args = wp_parse_args(
-				$args,
-				[
-					'title'           => '',
-					'priority'        => Customizer::get_priority( $args['section'] ),
-					'description'     => '',
-					'active_callback' => '',
-					'panel'           => $this->panel,
-				]
-			);
-			$this->wp_customize->add_section(
-				$args['section'],
-				$args
-			);
-			/**
-			 * セクション情報をセット
-			 */
-			$this->section = $args['section'];
-		}
+	public function add_image( $args ) {
+		$args = $this->parse_args( $args );
+		// 設定追加
+		$this->add_setting( $args, false );
+		$this->wp_customize->add_control(
+			new \WP_Customize_Image_Control(
+				$this->wp_customize,
+				$args['id'],
+				self::get_control_args( $args, $args['id'] )
+			)
+		);
+		$this->do_action_after_add_setting( $args['id'], $args );
 	}
 
 	/**
-	 * Add setting and control : color
+	 * カラー.
 	 *
 	 * @param array $args オプション.
 	 */
 	public function add_color( $args ) {
-		$args                      = $this->parse_args( $args );
+		$args = $this->parse_args( $args );
+		// サニタイズ設定.
 		$args['sanitize_callback'] = 'sanitize_hex_color';
-		$this->add_setting_and_control( $args, false );
+		// 設定追加.
+		$this->add_setting( $args, false );
+		// パレット設定.
 		$palettes      = true;
 		$color_palette = get_theme_support( 'editor-color-palette' );
 		if ( $color_palette && is_array( $color_palette ) ) {
 			$palettes = array_column( $color_palette[0], 'color' );
 		}
 
+		// コントロール追加.
 		if ( class_exists( __NAMESPACE__ . '\Color_Control' ) ) {
 			$this->wp_customize->add_control(
 				new Color_Control(
@@ -142,204 +232,102 @@ class Customize_Control {
 				)
 			);
 		}
+		$this->do_action_after_add_setting( $args['id'], $args );
 	}
 
 	/**
-	 * Add setting and control : image
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_image( $args ) {
-		$args = $this->parse_args( $args );
-		$this->add_setting_and_control( $args, false );
-		$this->wp_customize->add_control(
-			new \WP_Customize_Image_Control(
-				$this->wp_customize,
-				$args['id'],
-				self::get_control_args( $args, $args['id'] )
-			)
-		);
-	}
-
-	/**
-	 * Add setting and control : radio
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_radio( $args ) {
-		$args['control_type'] = 'radio';
-
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : select
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_select( $args ) {
-		$args['control_type']      = 'select';
-		$args['sanitize_callback'] = [ $this, 'sanitize_select' ];
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : label radio
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_image_label_radio( $args ) {
-		$args                      = $this->parse_args( $args );
-		$args['control_type']      = 'radio';
-		$args['sanitize_callback'] = [ $this, 'sanitize_select' ];
-		$this->add_setting_and_control( $args, false );
-		$this->wp_customize->add_control(
-			new Image_Label_Radio_Control(
-				$this->wp_customize,
-				$args['id'],
-				self::get_control_args( $args, $args['id'] )
-			)
-		);
-	}
-
-	/**
-	 * Add setting and control : checkbox
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_checkbox( $args ) {
-		$args['control_type']      = 'checkbox';
-		$args['sanitize_callback'] = [ $this, 'sanitize_checkbox' ];
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : text
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_text( $args ) {
-		$args['control_type'] = 'text';
-		if ( ! isset( $args['sanitize_callback'] ) ) {
-			$args['sanitize_callback'] = 'sanitize_text_field';
-		}
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : number
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_number( $args ) {
-		$args['control_type']      = 'number';
-		$args['sanitize_callback'] = [ $this, 'sanitize_number' ];
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : textarea
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_textarea( $args ) {
-		$args['control_type'] = 'textarea';
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : textarea(no html)
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_plain_textarea( $args ) {
-		/**
-		 * サニタイズのセット
-		 */
-		$args['sanitize_callback'] = [ $this, 'sanitize_plain_text' ];
-		$this->add_textarea( $args );
-	}
-
-	/**
-	 * Add setting and control : url
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_url( $args ) {
-		$args['control_type']      = 'url';
-		$args['sanitize_callback'] = 'esc_url_raw';
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : label
-	 *
-	 * @param array $args オプション.
-	 */
-	public function add_label( $args ) {
-		$args['control_type'] = 'hidden';
-		$args['transport']    = 'postMessage';
-		$this->add_setting_and_control( $args );
-	}
-
-	/**
-	 * Add setting and control : section label.
+	 * セクションラベル.
 	 *
 	 * @param string $label 文字.
-	 * @param array  $args  オプション.
+	 * @param array $args オプション.
 	 */
 	public function add_section_label( $label, $args = [] ) {
-		$args                 = $this->parse_args( $args );
+		$args = $this->parse_args( $args );
+		// 追加設定.
 		$args['control_type'] = 'hidden';
 		$args['transport']    = 'postMessage';
 		$args['label']        = $label;
+		// IDがなければ自動生成.
 		if ( ! isset( $args['id'] ) ) {
 			$args['id'] = 'ys_' . substr( md5( $label . $args['section'] ), 0, 40 );
 		}
-		$this->add_setting_and_control( $args, false );
-		$this->wp_customize->add_control(
-			new Section_Label_Control(
-				$this->wp_customize,
-				$args['id'],
-				self::get_control_args( $args, $args['id'] )
-			)
-		);
+		// 設定のみ追加.
+		$this->add_setting( $args, false );
+		// セクションラベルコントロールの追加.
+		if ( class_exists( __NAMESPACE__ . '\Section_Label_Control' ) ) {
+			$this->wp_customize->add_control(
+				new Section_Label_Control(
+					$this->wp_customize,
+					$args['id'],
+					self::get_control_args( $args, $args['id'] )
+				)
+			);
+			$this->do_action_after_add_setting( $args['id'], $args );
+		}
 	}
 
 	/**
-	 * 設定・コントロール追加
+	 * パネル追加
 	 *
-	 * @param array               $args    Args.
-	 * @param object|null|boolean $control Control.
-	 *
-	 * @return void
+	 * @param array $args パラメーター.
 	 */
-	private function add_setting_and_control( $args, $control = null ) {
-		$args = $this->parse_args( $args );
-		$id   = $args['id'];
-		/**
-		 * 設定値分割.
-		 */
-		$setting = self::get_setting_args( $args, $id );
-		if ( is_null( $control ) ) {
-			$control = self::get_control_args( $args, $id );
+	public function add_panel( $args ) {
+		if ( ! isset( $args['panel'] ) ) {
+			return;
 		}
-
-		$this->wp_customize->add_setting( $id, $setting );
-		if ( is_array( $control ) ) {
-			$this->wp_customize->add_control( $id, $control );
-		} elseif ( false !== $control ) {
-			$this->wp_customize->add_control( $control );
-		}
+		// デフォルト設定の追加.
+		$args = wp_parse_args(
+			$args,
+			[
+				'title'           => '',
+				'priority'        => Customizer::get_priority( $args['panel'] ),
+				'description'     => '',
+				'active_callback' => '',
+			]
+		);
+		// パネル追加.
+		$this->wp_customize->add_panel(
+			$args['panel'],
+			$args
+		);
+		//  パネル情報を保存.
+		$this->panel = $args['panel'];
 	}
 
+	/**
+	 * セクション追加
+	 *
+	 * @param array $args パラメーター.
+	 */
+	public function add_section( $args ) {
+		if ( ! isset( $args['section'] ) ) {
+			return;
+		}
+		$args = wp_parse_args(
+			$args,
+			[
+				'title'           => '',
+				'priority'        => Customizer::get_priority( $args['section'] ),
+				'description'     => '',
+				'active_callback' => '',
+				'panel'           => $this->panel,
+			]
+		);
+		// セクション追加.
+		$this->wp_customize->add_section(
+			$args['section'],
+			$args
+		);
+		// セクション情報を保存.
+		$this->section = $args['section'];
+	}
 
 	/**
 	 * 設定用パラメーターの抽出
 	 *
-	 * @param array  $args   パラメーター.
-	 * @param string $id     ID.
-	 * @param array  $option 追加パラメーター.
+	 * @param array $args パラメーター.
+	 * @param string $id ID.
+	 * @param array $option 追加パラメーター.
 	 *
 	 * @return array
 	 */
@@ -352,7 +340,7 @@ class Customize_Control {
 		];
 
 		return apply_filters(
-			'ys_customize_get_setting_args__' . $id,
+			"ys_customize_get_setting_args__{$id}",
 			array_merge(
 				self::parse_customize_args( $args, $setting_keys ),
 				$option
@@ -363,9 +351,9 @@ class Customize_Control {
 	/**
 	 * コントロール用パラメーターの抽出
 	 *
-	 * @param array  $args   パラメーター.
-	 * @param string $id     ID.
-	 * @param array  $option 追加パラメーター.
+	 * @param array $args パラメーター.
+	 * @param string $id ID.
+	 * @param array $option 追加パラメーター.
 	 *
 	 * @return array
 	 */
@@ -418,40 +406,37 @@ class Customize_Control {
 	 * @return array
 	 */
 	public function parse_args( $args ) {
-		$id = '';
-		/**
-		 * デフォルト指定チェック
-		 */
+		// 設定名を取得.
+		$setting_name = isset( $args['id'] ) ? $args['id'] : '';
+
+		// デフォルト指定チェック.
 		if ( ! isset( $args['default'] ) ) {
 			$args['default'] = '';
 		}
-		if ( isset( $args['id'] ) ) {
-			$id              = $args['id'];
-			$args['default'] = Option::get_default( $id, $args['default'] );
+		if ( ! empty( $setting_name ) ) {
+			$args['default'] = Option::get_default( $setting_name, $args['default'] );
 		}
 
-		/**
-		 * セクション
-		 */
+		// セクション指定チェック.
 		$args['section'] = isset( $args['section'] ) ? $args['section'] : $this->section;
 
 		return apply_filters(
-			'ys_customizer_parse_args__' . $id,
-			wp_parse_args( $args, $this->get_default_option() ),
+			"ys_customizer_parse_args__{$setting_name}",
+			wp_parse_args( $args, self::get_default_option() ),
 			$this->wp_customize,
-			$id
+			$setting_name
 		);
 	}
 
 	/**
 	 * カスタマイザーのデフォルト値を取得
 	 */
-	public function get_default_option() {
+	public static function get_default_option() {
 		return [
 			'setting_type'      => 'option',
 			'description'       => '',
 			'transport'         => 'refresh',
-			'priority'          => 1,
+			'priority'          => 10,
 			'default'           => '',
 			'input_attrs'       => [],
 			'sanitize_callback' => '',
@@ -462,7 +447,7 @@ class Customize_Control {
 	/**
 	 * 設定のセクションを変更にする
 	 *
-	 * @param string $name    Name.
+	 * @param string $name Name.
 	 * @param string $section Section.
 	 */
 	public function set_section( $name, $section ) {
@@ -498,9 +483,55 @@ class Customize_Control {
 	}
 
 	/**
+	 * 設定・コントロール追加
+	 *
+	 * @param array $args Args.
+	 * @param object|null|boolean $control Control.
+	 *
+	 * @return void
+	 */
+	private function add_setting( $args, $control = null ) {
+		$args         = $this->parse_args( $args );
+		$setting_name = $args['id'];
+		// 設定値を分割.
+		$setting = self::get_setting_args( $args, $setting_name );
+		// コントロールの指定がなければ設定から変換してセット.
+		if ( is_null( $control ) ) {
+			$control = self::get_control_args( $args, $setting_name );
+		}
+		// 設定追加.
+		$this->wp_customize->add_setting( $setting_name, $setting );
+		// コントロール追加.
+		$add_control = false;
+		if ( is_array( $control ) ) {
+			$this->wp_customize->add_control( $setting_name, $control );
+			$add_control = true;
+		} elseif ( false !== $control ) {
+			$this->wp_customize->add_control( $control );
+			$add_control = true;
+		}
+		// コントロール追加していたらアクションを実行.
+		if ( $add_control ) {
+			$this->do_action_after_add_setting( $setting_name, $args );
+		}
+	}
+
+	/**
+	 * 設定・コントロール追加後のアクション
+	 *
+	 * @param string $setting Setting Name.
+	 * @param array $args Args.
+	 *
+	 * @return void
+	 */
+	public function do_action_after_add_setting( $setting, $args ) {
+		do_action( "ys_customizer_add_setting__{$setting}", $this->wp_customize, $args );
+	}
+
+	/**
 	 * Select,radio
 	 *
-	 * @param string $input   input.
+	 * @param string $input input.
 	 * @param object $setting setting.
 	 *
 	 * @return string
@@ -515,7 +546,7 @@ class Customize_Control {
 	/**
 	 * Checkbox
 	 *
-	 * @param string $value   input.
+	 * @param string $value input.
 	 * @param object $setting setting.
 	 *
 	 * @return bool
@@ -531,7 +562,7 @@ class Customize_Control {
 	/**
 	 * Number
 	 *
-	 * @param string $number  number.
+	 * @param string $number number.
 	 * @param object $setting setting.
 	 *
 	 * @return int
