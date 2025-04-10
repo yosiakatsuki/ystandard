4055a6c156020f50a2a21c851e8b487edd0039d7
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-04-10 11:55:44 +0900

    ブロックエディター向けCSS読み込み調整

--- inc/block-editor/class-block-editor-assets.php
+++ inc/block-editor/class-block-editor-assets.php
@@ -27,17 +27,23 @@ class Block_Editor_Assets {
 	 * Block_Editor_Assets constructor.
 	 */
 	public function __construct() {
-		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_assets' ], 11 );
-		add_action( 'after_setup_theme', [ $this, 'enqueue_block_css' ] );
+		add_action( 'enqueue_block_assets', [ $this, 'enqueue_block_editor_assets' ], 11 );
+		add_action( 'after_setup_theme', [ $this, 'enqueue_editor_style' ] );
 	}
 
 	/**
 	 * ブロックエディタのスタイル追加
 	 */
 	public function enqueue_block_editor_assets() {
+		if ( ! is_admin() ) {
+			return;
+		}
+		$file = '/css/block-editor-assets.css';
 		wp_enqueue_style(
 			'ys-block-editor-assets',
-			get_template_directory_uri() . '/css/block-editor-assets.css'
+			get_template_directory_uri() . $file,
+			[],
+			filemtime( get_template_directory() . $file )
 		);
 		$custom_property        = Enqueue_Styles::get_css_vars_selector( 'body .editor-styles-wrapper' );
 		$custom_property_preset = Enqueue_Styles::get_css_vars_selector_preset( 'body .editor-styles-wrapper' );
@@ -50,7 +56,7 @@ class Block_Editor_Assets {
 	/**
 	 * Enqueue block editor style
 	 */
-	public function enqueue_block_css() {
+	public function enqueue_editor_style() {
 		add_theme_support( 'editor-styles' );
 		add_editor_style( 'css/block-editor.css' );
 		add_editor_style( 'style.css' );
