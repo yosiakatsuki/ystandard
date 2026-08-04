6e9d3009990d7a2e5a2db80d295803391f0f098f
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-08-04 15:51:16 +0900

    ブロックCSSの読み込み順を調整

--- inc/blocks/class-blocks.php
+++ inc/blocks/class-blocks.php
@@ -25,30 +25,43 @@ class Blocks {
 	 */
 	private $enqueue_block_editor_styles;
 	/**
-	 * Styles.
+	 * 必須スタイル.
 	 *
 	 * @var array
 	 */
-	private $enqueue_styles;
+	private $required_styles;
+	/**
+	 * 必須スタイル名.
+	 *
+	 * @var array
+	 */
+	private $required_style_names;
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
 		$this->enqueue_block_editor_styles = [];
-		$this->enqueue_styles              = [];
-		add_action( 'enqueue_block_assets', [ $this, 'enqueue_theme_block_styles' ] );
-		add_action( 'enqueue_block_assets', [ $this, 'enqueue_styles' ], 100 );
+		$this->required_styles             = [];
+		$this->required_style_names        = [];
+		add_action( 'init', [ $this, 'register_theme_block_styles' ], 20 );
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_required_styles' ], 20 );
 		add_action( 'enqueue_block_assets', [ $this, 'enqueue_block_editor_styles' ], 100 );
 	}
 
 	/**
-	 * Enqueue Block Styles
+	 * ブロックスタイル登録.
 	 *
 	 * @return void
 	 */
-	public function enqueue_theme_block_styles() {
-		$this->enqueue_block_styles( get_template_directory() . '/css/block-styles' );
+	public function register_theme_block_styles() {
+		// WordPress 7.0以降のオンデマンド設定を登録前に反映.
+		wp_styles();
+
+		$required_style_names       = apply_filters( 'ys_required_block_style_names', [] );
+		$this->required_style_names = is_array( $required_style_names ) ? $required_style_names : [];
+
+		$this->register_block_styles( get_template_directory() . '/css/block-styles' );
 		// フックで追加.
 		$style_dir_path = apply_filters( 'ys_block_styles_dir_path', [] );
 		// 子テーマも検索.
@@ -58,7 +71,7 @@ class Blocks {
 		if ( is_array( $style_dir_path ) && ! empty( $style_dir_path ) ) {
 			$style_dir_path = array_values( array_unique( $style_dir_path ) );
 			foreach ( $style_dir_path as $path ) {
-				$this->enqueue_block_styles( $path );
+				$this->register_block_styles( $path );
 			}
 		}
 	}
@@ -70,8 +83,13 @@ class Blocks {
 	 *
 	 * @return void
 	 */
-	private function enqueue_block_styles( $dir ) {
-		foreach ( glob( "{$dir}/*", GLOB_ONLYDIR ) as $dir_path ) {
+	private function register_block_styles( $dir ) {
+		$dir_paths = glob( "{$dir}/*", GLOB_ONLYDIR );
+		if ( false === $dir_paths ) {
+			return;
+		}
+
+		foreach ( $dir_paths as $dir_path ) {
 			$block        = $this->parse_block_name( basename( $dir_path ) );
 			$block_name   = $block['namespace'] . '/' . $block['name'];
 			$style_handle = "ystd-$block_name";
@@ -82,25 +100,18 @@ class Blocks {
 			$theme_css_path = $dir_path . '/' . $block['name'] . '.css';
 			if ( file_exists( $theme_css_path ) ) {
 				// URL.
-				$style_src              = $this->replace_path_to_uri( $theme_css_path, $is_child );
-				$this->enqueue_styles[] = [
+				$style_src  = $this->replace_path_to_uri( $theme_css_path, $is_child );
+				$style_args = [
 					'handle' => $style_handle,
 					'src'    => $style_src,
+					'ver'    => filemtime( $theme_css_path ),
 					'path'   => $theme_css_path,
 				];
-			}
+				wp_enqueue_block_style( $block_name, $style_args );
 
-			// 必ず読み込むスタイル.
-			$required = apply_filters( 'ys_required_block_style_names', [] );
-			if ( is_array( $required ) ) {
-				foreach ( $required as $item ) {
-					if ( false !== strpos( $block['name'], $item ) ) {
-						$this->enqueue_styles[] = [
-							'handle' => $style_handle,
-							'src'    => $style_src,
-							'path'   => $theme_css_path,
-						];
-					}
+				// 必ず読み込むスタイル.
+				if ( $this->is_required_style( $block['name'] ) ) {
+					$this->required_styles[ $style_handle ] = $style_args;
 				}
 			}
 
@@ -121,18 +132,35 @@ class Blocks {
 	}
 
 	/**
-	 * ブロックCSS
+	 * 必ず読み込むブロックCSS
 	 *
 	 * @return void
 	 */
-	public function enqueue_styles() {
-		if ( ! is_array( $this->enqueue_styles ) ) {
+	public function enqueue_required_styles() {
+		if ( ! is_array( $this->required_styles ) ) {
 			return;
 		}
-		foreach ( $this->enqueue_styles as $style ) {
+		foreach ( $this->required_styles as $style ) {
 			$this->enqueue_style( $style['handle'], $style['src'], $style['path'] );
 		}
-		$this->enqueue_styles = [];
+		$this->required_styles = [];
+	}
+
+	/**
+	 * 必ず読み込むスタイルか.
+	 *
+	 * @param string $block_name ブロック名.
+	 *
+	 * @return bool
+	 */
+	private function is_required_style( $block_name ) {
+		foreach ( $this->required_style_names as $required_style_name ) {
+			if ( false !== strpos( $block_name, $required_style_name ) ) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	/**
